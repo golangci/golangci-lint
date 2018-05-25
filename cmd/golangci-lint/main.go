d86489893d9e1c6304312aaf94c8b854a7e89c43
golangci <dev@golangci.com>
2018-05-25 22:30:44 +0300

    move log handling code into root command

--- cmd/golangci-lint/main.go
+++ cmd/golangci-lint/main.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"log"
-
 	"github.com/golangci/golangci-lint/pkg/commands"
-	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	log.SetFlags(0) // don't print time
-	logrus.SetLevel(logrus.WarnLevel)
-
 	e := commands.NewExecutor()
 	if err := e.Execute(); err != nil {
 		panic(err)
