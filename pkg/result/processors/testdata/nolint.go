cad9fc7760e124b822d14cfed508648cd5cc2de6
golangci <dev@golangci.com>
2018-05-28 12:19:55 +0300

    #26: fix spaces in nolint directive

--- pkg/result/processors/testdata/nolint.go
+++ pkg/result/processors/testdata/nolint.go
@@ -1,5 +1,7 @@
 package testdata
 
 var nolintSpecific int    // nolint:gofmt
+var nolintSpace int       // nolint: gofmt
+var nolintSpaces int      //nolint: gofmt, govet
 var nolintAll int         // nolint
-var nolintAndAppendix int // nolint Some My Text
+var nolintAndAppendix int // nolint // another comment
