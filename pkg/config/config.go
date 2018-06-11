1ff1d6733fe5163905124498620a9958113e0179
Denis Isaev <denis@golangci.com>
2018-06-11 18:57:33 +0300

    ignore 'comment on exported type ... should be of the form' by default

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -28,7 +28,7 @@ var DefaultExcludePatterns = []ExcludePattern{
 		Why:     "Almost all programs ignore errors on these functions and in most cases it's ok",
 	},
 	{
-		Pattern: "(comment on exported (method|function)|should have( a package)? comment|comment should be of the form)",
+		Pattern: "(comment on exported (method|function|type)|should have( a package)? comment|comment should be of the form)",
 		Linter:  "golint",
 		Why:     "Annoying issue about not having a comment. The rare codebase has such comments",
 	},
