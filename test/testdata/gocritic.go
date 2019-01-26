3e9b681cf0a9af051dd6f3ed532187d87540d5f1
Denis Isaev <denis@golangci.com>
2019-01-26 18:26:38 +0300

    fix #370: fix go-critic configuration broken in v1.13

--- test/testdata/gocritic.go
+++ test/testdata/gocritic.go
@@ -1,6 +1,31 @@
 //args: -Egocritic
+//config_path: testdata/configs/gocritic.yml
 package testdata
 
-import "flag"
+import (
+	"flag"
+	"log"
+)
 
 var _ = *flag.Bool("global1", false, "") // ERROR "flagDeref: immediate deref in \*flag.Bool\(.global1., false, ..\) is most likely an error; consider using flag\.BoolVar"
+
+type size1 struct {
+	a bool
+}
+
+type size2 struct {
+	size1
+	b bool
+}
+
+func gocriticRangeValCopySize1(ss []size1) {
+	for _, s := range ss {
+		log.Print(s)
+	}
+}
+
+func gocriticRangeValCopySize2(ss []size2) {
+	for _, s := range ss { // ERROR "rangeValCopy: each iteration copies 2 bytes.*"
+		log.Print(s)
+	}
+}
