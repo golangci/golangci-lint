1eb712544cd7c11bdb8c5e857cb85c6ded6a173f
Viacheslav Poturaev <vearutop@gmail.com>
2019-02-14 11:43:19 +0100

    Add version information to built artifact when go get with go1.12

---
+++ cmd/golangci-lint/mod_version.go
@@ -0,0 +1,18 @@
+// +build go1.12
+
+package main
+
+import (
+	"fmt"
+	"runtime/debug"
+)
+
+func init() {
+	if info, available := debug.ReadBuildInfo(); available {
+		if date == "" && info.Main.Version != "(devel)" {
+			version = info.Main.Version
+			commit = fmt.Sprintf("(unknown, mod sum: %q)", info.Main.Sum)
+			date = "(unknown)"
+		}
+	}
+}
