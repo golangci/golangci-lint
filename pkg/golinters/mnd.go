35772948edd8b70e3ce380b3a9ddda8d9a3a7051
sayboras <sayboras@yahoo.com>
2019-11-05 22:42:05 +1100

    Fixed goimports

--- pkg/golinters/mnd.go
+++ pkg/golinters/mnd.go
@@ -4,7 +4,7 @@ import (
 	"golang.org/x/tools/go/analysis"
 
 	"github.com/golangci/golangci-lint/pkg/golinters/goanalysis"
-	"github.com/tommy-muehle/go-mnd"
+	magic_numbers "github.com/tommy-muehle/go-mnd"
 )
 
 func NewMnd() *goanalysis.Linter {
