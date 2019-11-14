0e5b7a04d583e58e7628810918b9dc77aa870584
Simon Sawert <simon@sawert.se>
2019-11-14 12:12:32 +0100

    Bump wsl to v2.0.0

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.12
 
 require (
 	github.com/OpenPeeDeeP/depguard v1.0.1
-	github.com/bombsimon/wsl v1.2.5
+	github.com/bombsimon/wsl/v2 v2.0.0
 	github.com/fatih/color v1.7.0
 	github.com/go-critic/go-critic v0.3.5-0.20190904082202-d79a9f0c64db
 	github.com/go-lintpack/lintpack v0.5.2
@@ -41,8 +41,8 @@ require (
 	github.com/ultraware/whitespace v0.0.4
 	github.com/uudashr/gocognit v0.0.0-20190926065955-1655d0de0517
 	github.com/valyala/quicktemplate v1.2.0
-	golang.org/x/tools v0.0.0-20191010075000-0337d82405ff
-	gopkg.in/yaml.v2 v2.2.4
+	golang.org/x/tools v0.0.0-20191113232020-e2727e816f5a
+	gopkg.in/yaml.v2 v2.2.5
 	honnef.co/go/tools v0.0.1-2019.2.3
 	mvdan.cc/interfacer v0.0.0-20180901003855-c20040233aed
 	mvdan.cc/lint v0.0.0-20170908181259-adc824a0674b // indirect
--- pkg/golinters/wsl.go
+++ pkg/golinters/wsl.go
@@ -3,7 +3,7 @@ package golinters
 import (
 	"sync"
 
-	"github.com/bombsimon/wsl"
+	"github.com/bombsimon/wsl/v2"
 	"golang.org/x/tools/go/analysis"
 
 	"github.com/golangci/golangci-lint/pkg/golinters/goanalysis"
@@ -37,13 +37,14 @@ func NewWSL() *goanalysis.Linter {
 				files        = []string{}
 				linterCfg    = lintCtx.Cfg.LintersSettings.WSL
 				processorCfg = wsl.Configuration{
-					StrictAppend:                linterCfg.StrictAppend,
-					AllowAssignAndCallCuddle:    linterCfg.AllowAssignAndCallCuddle,
-					AllowMultiLineAssignCuddle:  linterCfg.AllowMultiLineAssignCuddle,
-					AllowCaseTrailingWhitespace: linterCfg.AllowCaseTrailingWhitespace,
-					AllowCuddleDeclaration:      linterCfg.AllowCuddleDeclaration,
-					AllowCuddleWithCalls:        []string{"Lock", "RLock"},
-					AllowCuddleWithRHS:          []string{"Unlock", "RUnlock"},
+					StrictAppend:                     linterCfg.StrictAppend,
+					AllowAssignAndCallCuddle:         linterCfg.AllowAssignAndCallCuddle,
+					AllowMultiLineAssignCuddle:       linterCfg.AllowMultiLineAssignCuddle,
+					AllowCuddleDeclaration:           linterCfg.AllowCuddleDeclaration,
+					AllowTrailingComment:             linterCfg.AllowTrailingComment,
+					CaseForceTrailingWhitespaceLimit: linterCfg.CaseForceTrailingWhitespaceLimit,
+					AllowCuddleWithCalls:             []string{"Lock", "RLock"},
+					AllowCuddleWithRHS:               []string{"Unlock", "RUnlock"},
 				}
 			)
 
