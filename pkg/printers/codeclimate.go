3e05cb7217daa4175da04aa99c75c7c9ed272141
☃ Elliot Shepherd <elliot@identitii.com>
2020-05-09 00:06:14 +1000

    Fix nil pointer in codeclimate.go
    Some issues don't have any source lines

--- pkg/printers/codeclimate.go
+++ pkg/printers/codeclimate.go
@@ -39,9 +39,14 @@ func (p CodeClimate) Print(ctx context.Context, issues []result.Issue) error {
 		issue.Location.Path = i.Pos.Filename
 		issue.Location.Lines.Begin = i.Pos.Line
 
-		// Need a checksum of the issue, so we use MD5 of the filename, text, and first line of source
+		// Need a checksum of the issue, so we use MD5 of the filename, text, and first line of source if there is any
+		var firstLine string
+		if len(i.SourceLines) > 0 {
+			firstLine = i.SourceLines[0]
+		}
+
 		hash := md5.New() //nolint:gosec
-		_, _ = hash.Write([]byte(i.Pos.Filename + i.Text + i.SourceLines[0]))
+		_, _ = hash.Write([]byte(i.Pos.Filename + i.Text + firstLine))
 		issue.Fingerprint = fmt.Sprintf("%X", hash.Sum(nil))
 
 		allIssues = append(allIssues, issue)
