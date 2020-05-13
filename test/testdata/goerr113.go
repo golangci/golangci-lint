6163dec876ffe8842a6656b6a4ea37737bc125cf
Denis Isaev <idenx@yandex.com>
2020-05-13 21:16:39 +0300

    fix goerr13 dependencies

--- test/testdata/goerr113.go
+++ test/testdata/goerr113.go
@@ -1,10 +1,21 @@
 //args: -Egoerr113
 package testdata
 
+import "os"
+
 func SimpleEqual(e1, e2 error) bool {
 	return e1 == e2 // ERROR `err113: do not compare errors directly, use errors.Is() instead: "e1 == e2"`
 }
 
 func SimpleNotEqual(e1, e2 error) bool {
 	return e1 != e2 // ERROR `err113: do not compare errors directly, use errors.Is() instead: "e1 != e2"`
+}
+
+func CheckGoerr13Import(e error) bool {
+	f, err := os.Create("f.txt")
+	if err != nil {
+		return err == e  // ERROR `err113: do not compare errors directly, use errors.Is() instead: "err == e"`
+	}
+	f.Close()
+	return false
 }
