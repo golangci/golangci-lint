7c2871c5968d16cdd303dbc6dc31e016e8611697
Denis Krivak <dokrivak@avito.ru>
2020-05-18 15:49:58 +0300

    godot: add autofix mode

---
+++ test/testdata/fix/out/godot.go
@@ -0,0 +1,12 @@
+//args: -Egodot
+package p
+
+/*
+This comment won't be checked in default mode
+*/
+
+// This comment will be fixed.
+func godot(a, b int) int {
+	// Nothing to do here
+	return a + b
+}
