a60b4ee9eb4bf9bb800e6008b118f4714849d036
Simon Sawert <simon@sawert.se>
2020-10-11 23:41:11 +0200

    Always include include tests in the analyser

--- pkg/golinters/gochecknoglobals.go
+++ pkg/golinters/gochecknoglobals.go
@@ -1,6 +1,8 @@
 package golinters
 
 import (
+	"flag"
+
 	"golang.org/x/tools/go/analysis"
 
 	"4d63.com/gochecknoglobals/checknoglobals"
@@ -9,7 +11,15 @@ import (
 )
 
 func NewGochecknoglobals() *goanalysis.Linter {
+	// gochecknoglobals only lints test files if the `-t` flag is passed so we
+	// set up our own FlagSet and add it to the analyzer before running it. This
+	// can be turned of by using the regular golangci-lint flags such as
+	// `--tests` or `--skip-files`.
+	flags := flag.NewFlagSet("", flag.ContinueOnError)
+	flags.Bool("t", true, "Include tests")
+
 	gochecknoglobals := checknoglobals.Analyzer()
+	gochecknoglobals.Flags = *flags
 
 	return goanalysis.NewLinter(
 		gochecknoglobals.Name,
