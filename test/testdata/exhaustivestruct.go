55e35d27e3dd54f6249710cb0308760d526a7bfa
Mateusz Bilski <mateusz.bilski@gmail.com>
2020-10-27 00:54:55 +0100

    Update exhaustivestruct (#1472)
    * Update exhaustivestruct
    
    * Add tests

--- test/testdata/exhaustivestruct.go
+++ test/testdata/exhaustivestruct.go
@@ -4,13 +4,21 @@ package testdata
 type Test struct {
 	A string
 	B int
+	c bool // ignore private field
+	D float64
 }
 
 var pass = Test{
 	A: "a",
 	B: 0,
+	D: 1.0,
 }
 
 var fail = Test{ // ERROR "B is missing in Test"
+	A: "a",
+	D: 1.0,
+}
+
+var failMultiple = Test{ // ERROR "B, D are missing in Test"
 	A: "a",
 }
