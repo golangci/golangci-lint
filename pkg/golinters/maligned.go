8946e20ebaf3a6cdff3495f18ec56e99bb051f93
Maciej "Iwan" Iwanowski <maciej.iwanowski@critical.today>
2020-12-13 00:17:06 +0100

    Using upstream maligned and updating error message

--- pkg/golinters/maligned.go
+++ pkg/golinters/maligned.go
@@ -1,72 +1,17 @@
 package golinters
 
 import (
-	//"fmt"
-	//"sync"
-
-	"fmt"
-
 	"github.com/mdempsky/maligned/passes/maligned"
 	"golang.org/x/tools/go/analysis"
 
 	"github.com/golangci/golangci-lint/pkg/golinters/goanalysis"
-	"github.com/golangci/golangci-lint/pkg/lint/linter"
-	//"github.com/golangci/golangci-lint/pkg/result"
-
-	//"github.com/golangci/golangci-lint/pkg/lint/linter"
-	//"github.com/golangci/golangci-lint/pkg/result"
 )
 
 func NewMaligned() *goanalysis.Linter {
 	return goanalysis.NewLinter(
 		"maligned",
-		"",
+		maligned.Analyzer.Doc,
 		[]*analysis.Analyzer{maligned.Analyzer},
 		nil,
-	).WithLoadMode(goanalysis.LoadModeTypesInfo).WithIssuesReporter(func(ctx *linter.Context) []goanalysis.Issue {
-		fmt.Print(ctx.Packages[0].)
-		return nil
-	})
-	//const linterName = "maligned"
-	//var mu sync.Mutex
-	//var res []goanalysis.Issue
-	//analyzer := &analysis.Analyzer{
-	//	Name: linterName,
-	//	Doc:  goanalysis.TheOnlyanalyzerDoc,
-	//}
-	//return goanalysis.NewLinter(
-	//	linterName,
-	//	"Tool to detect Go structs that would take less memory if their fields were sorted",
-	//	[]*analysis.Analyzer{analyzer},
-	//	nil,
-	//).WithContextSetter(func(lintCtx *linter.Context) {
-	//	analyzer.Run = func(pass *analysis.Pass) (interface{}, error) {
-	//		prog := goanalysis.MakeFakeLoaderProgram(pass)
-	//
-	//		malignedIssues := malignedAPI.Run(prog)
-	//		if len(malignedIssues) == 0 {
-	//			return nil, nil
-	//		}
-	//
-	//		issues := make([]goanalysis.Issue, 0, len(malignedIssues))
-	//		for _, i := range malignedIssues {
-	//			text := fmt.Sprintf("struct of size %d bytes could be of size %d bytes", i.OldSize, i.NewSize)
-	//			if lintCtx.Settings().Maligned.SuggestNewOrder {
-	//				text += fmt.Sprintf(":\n%s", formatCodeBlock(i.NewStructDef, lintCtx.Cfg))
-	//			}
-	//			issues = append(issues, goanalysis.NewIssue(&result.Issue{
-	//				Pos:        i.Pos,
-	//				Text:       text,
-	//				FromLinter: linterName,
-	//			}, pass))
-	//		}
-	//
-	//		mu.Lock()
-	//		res = append(res, issues...)
-	//		mu.Unlock()
-	//		return nil, nil
-	//	}
-	//}).WithIssuesReporter(func(*linter.Context) []goanalysis.Issue {
-	//	return res
-	//}).WithLoadMode(goanalysis.LoadModeTypesInfo)
+	).WithLoadMode(goanalysis.LoadModeTypesInfo)
 }
--- test/testdata/maligned.go
+++ test/testdata/maligned.go
@@ -1,7 +1,7 @@
 //args: -Emaligned
 package testdata
 
-type BadAlignedStruct struct { // ERROR "struct of size 24 bytes could be of size 16 bytes"
+type BadAlignedStruct struct { // ERROR "struct of size 24 could be 16"
 	B  bool
 	I  int
 	B2 bool
