86df4e3b5027d363daeb270ebbfad6c3370814ce
Fernandez Ludovic <ldez@users.noreply.github.com>
2020-12-31 15:37:55 +0100

    feat: new unused implementation.

--- test/testdata/unused.go
+++ test/testdata/unused.go
@@ -1,6 +1,19 @@
 //args: -Eunused
 package testdata
 
+func fn1() {} // ERROR "func `fn1` is unused"
+
+//nolint:unused
+func fn2() { fn3() }
+
+func fn3() {} // ERROR "func `fn3` is unused"
+
+func fn4() { fn5() } // ERROR "func `fn4` is unused"
+
+func fn5() {} // ERROR "func `fn5` is unused"
+
+func fn6() { fn4() } // ERROR "func `fn6` is unused"
+
 type unusedStruct struct{} // ERROR "type `unusedStruct` is unused"
 
 type unusedStructNolintUnused struct{} //nolint:unused
