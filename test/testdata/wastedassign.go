1139493d28f51c613aacad1c0124242f38b07fc9
sanposhiho <handbomusic@gmail.com>
2021-01-13 18:36:17 +0900

    Fix test functions name

--- test/testdata/wastedassign.go
+++ test/testdata/wastedassign.go
@@ -27,7 +27,7 @@ func noUseParams(params string) int {
 	return a
 }
 
-func f(param int) int {
+func manyif(param int) int {
 	println(param)
 	useOutOfIf := 1212121 // ERROR "wasted assignment"
 	ret := 0
@@ -82,30 +82,7 @@ func checkLoopTest() int {
 	return hoge
 }
 
-func r(param int) int {
-	println(param)
-	useOutOfIf := 1212121
-	ret := 0
-	if false {
-		useOutOfIf = 200 // ERROR "reassigned, but never used afterwards"
-		return 0
-	} else if param == 100 {
-		ret = useOutOfIf
-	} else if param == 200 {
-		useOutOfIf = 100 // ERROR "wasted assignment"
-		useOutOfIf = 100
-		useOutOfIf = p(useOutOfIf)
-		useOutOfIf += 200 // ERROR "wasted assignment"
-	}
-	useOutOfIf = 12
-	println(useOutOfIf)
-	useOutOfIf = 192
-	useOutOfIf += 100
-	useOutOfIf += 200 // ERROR "reassigned, but never used afterwards"
-	return ret
-}
-
-func mugen() {
+func infinity() {
 	var i int
 	var hoge int
 	for {
@@ -117,7 +94,7 @@ func mugen() {
 	return
 }
 
-func noMugen() {
+func infinity2() {
 	var i int
 	var hoge int
 	for {
