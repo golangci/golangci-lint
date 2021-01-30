6b14958def11a97a241145ffd3a61b448c6dbe5c
jingyugao <1121087373@qq.com>
2021-01-30 09:36:10 +0800

    fix rowserrcheck reports false positive #943

--- test/testdata/rowserrcheck.go
+++ test/testdata/rowserrcheck.go
@@ -3,11 +3,63 @@ package testdata
 
 import (
 	"database/sql"
+	"fmt"
+	"math/rand"
 )
 
 func RowsErrNotChecked(db *sql.DB) {
 	rows, _ := db.Query("select id from tb") // ERROR "rows.Err must be checked"
 	for rows.Next() {
 
+	}
+}
+
+func issue943_1() {
+	db, err := sql.Open("postgres", "postgres://localhost:5432/postgres")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	var rows *sql.Rows
+	if rand.Float64() < 0.5 {
+		rows, err = db.Query("select 1")
+	} else {
+		rows, err = db.Query("select 2")
+	}
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		fmt.Println("new rows")
+	}
+	if err := rows.Err(); err != nil {
+		panic(err)
+	}
+}
+
+func issue943_1() {
+	db, err := sql.Open("postgres", "postgres://localhost:5432/postgres")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	var rows *sql.Rows
+	if rand.Float64() < 0.5 {
+		rows, err = db.Query("select 1") // ERROR "rows.Err must be checked"
+	} else {
+		rows, err = db.Query("select 2") // ERROR "rows.Err must be checked"
+	}
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		fmt.Println("new rows")
+	}
+	if err := rows.Err(); err != nil {
+		panic(err)
 	}
 }
