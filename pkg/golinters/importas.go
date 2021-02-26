4ccd36e1bcb5287585f910a16b418e80f7e01f03
Fernandez Ludovic <ldez@users.noreply.github.com>
2021-02-26 19:52:06 +0100

    review: don't panic :)

--- pkg/golinters/importas.go
+++ pkg/golinters/importas.go
@@ -8,22 +8,27 @@ import (
 
 	"github.com/golangci/golangci-lint/pkg/config"
 	"github.com/golangci/golangci-lint/pkg/golinters/goanalysis"
+	"github.com/golangci/golangci-lint/pkg/lint/linter"
 )
 
-func NewImportAs(cfg *config.ImportAsSettings) *goanalysis.Linter {
+func NewImportAs(settings *config.ImportAsSettings) *goanalysis.Linter {
 	analyzer := importas.Analyzer
-	if cfg != nil {
-		for alias, pkg := range *cfg {
-			if err := analyzer.Flags.Set("alias", fmt.Sprintf("%s:%s", pkg, alias)); err != nil {
-				panic(err.Error())
-			}
-		}
-	}
 
 	return goanalysis.NewLinter(
 		analyzer.Name,
 		analyzer.Doc,
 		[]*analysis.Analyzer{analyzer},
 		nil,
-	).WithLoadMode(goanalysis.LoadModeTypesInfo)
+	).WithContextSetter(func(lintCtx *linter.Context) {
+		if settings == nil {
+			return
+		}
+
+		for alias, pkg := range *settings {
+			err := analyzer.Flags.Set("alias", fmt.Sprintf("%s:%s", pkg, alias))
+			if err != nil {
+				lintCtx.Log.Errorf("failed to parse configuration: %v", err)
+			}
+		}
+	}).WithLoadMode(goanalysis.LoadModeTypesInfo)
 }
