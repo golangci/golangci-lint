9d4921de4327b36dee02f50842666c7f6a704ae0
Fernandez Ludovic <ldez@users.noreply.github.com>
2021-03-16 13:11:03 +0100

    add comment on constants.

--- pkg/lint/linter/config.go
+++ pkg/lint/linter/config.go
@@ -5,19 +5,19 @@ import (
 )
 
 const (
-	PresetBugs        = "bugs"
-	PresetComment     = "comment"
-	PresetComplexity  = "complexity"
-	PresetError       = "error"
-	PresetFormatting  = "format"
-	PresetImport      = "import"
-	PresetMetaLinter  = "metalinter"
-	PresetModule      = "module"
-	PresetPerformance = "performance"
-	PresetSQL         = "sql"
-	PresetStyle       = "style"
-	PresetTest        = "test"
-	PresetUnused      = "unused"
+	PresetBugs        = "bugs"        // Related to bugs detection.
+	PresetComment     = "comment"     // Related to comments analysis.
+	PresetComplexity  = "complexity"  // Related to code complexity analysis.
+	PresetError       = "error"       // Related to error handling analysis.
+	PresetFormatting  = "format"      // Related to code formatting.
+	PresetImport      = "import"      // Related to imports analysis.
+	PresetMetaLinter  = "metalinter"  // Related to linter that contains multiple rules or multiple linters.
+	PresetModule      = "module"      // Related to Go modules analysis.
+	PresetPerformance = "performance" // Related to performance.
+	PresetSQL         = "sql"         // Related to SQL.
+	PresetStyle       = "style"       // Related to coding style.
+	PresetTest        = "test"        // Related to the analysis of the code of the tests.
+	PresetUnused      = "unused"      // Related to the detection of unused code.
 )
 
 type Config struct {
