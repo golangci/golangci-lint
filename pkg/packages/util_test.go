f2487aeb11467e6b24076ecb36e5bb655ea39e45
Fernandez Ludovic <ldez@users.noreply.github.com>
2021-03-27 19:41:49 +0100

    fix: improve error stack parsing.

--- pkg/packages/util_test.go
+++ pkg/packages/util_test.go
@@ -28,6 +28,16 @@ func Test_stackCrusher(t *testing.T) {
 			stack:    `/home/ldez/sources/go/src/github.com/golangci/golangci-lint/pkg/golinters/deadcode.go:20:32: cannot use mu (variable of type sync.Mutex) as goanalysis.Issue value in argument to append`,
 			expected: "/home/ldez/sources/go/src/github.com/golangci/golangci-lint/pkg/golinters/deadcode.go:20:32: cannot use mu (variable of type sync.Mutex) as goanalysis.Issue value in argument to append",
 		},
+		{
+			desc:     "stack with message with parenthesis at the end",
+			stack:    `/home/username/childapp/interfaces/IPanel.go:4:2: could not import github.com/gotk3/gotk3/gtk (/home/username/childapp/vendor/github.com/gotk3/gotk3/gtk/aboutdialog.go:5:8: could not import C (cgo preprocessing failed))`,
+			expected: "/home/username/childapp/vendor/github.com/gotk3/gotk3/gtk/aboutdialog.go:5:8: could not import C (cgo preprocessing failed)",
+		},
+		{
+			desc:     "no stack but message with parenthesis at the end",
+			stack:    `/home/ldez/sources/go/src/github.com/golangci/sandbox/main.go:11:17: ui.test undefined (type App has no field or method test)`,
+			expected: "/home/ldez/sources/go/src/github.com/golangci/sandbox/main.go:11:17: ui.test undefined (type App has no field or method test)",
+		},
 	}
 
 	for _, test := range testCases {
