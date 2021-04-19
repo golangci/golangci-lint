9cb902cdf2944a19f3da44d49a999bc7480d28bc
Ludovic Fernandez <ldez@users.noreply.github.com>
2021-04-19 15:20:49 +0200

    fix: comma in exclude pattern leads to unexpected results (#1917)

--- pkg/commands/run.go
+++ pkg/commands/run.go
@@ -313,8 +313,15 @@ func fixSlicesFlags(fs *pflag.FlagSet) {
 			return
 		}
 
+		var safe []string
+		for _, v := range s {
+			// add quotes to escape comma because spf13/pflag use a CSV parser:
+			// https://github.com/spf13/pflag/blob/85dd5c8bc61cfa382fecd072378089d4e856579d/string_slice.go#L43
+			safe = append(safe, `"`+v+`"`)
+		}
+
 		// calling Set sets Changed to true: next Set calls will append, not overwrite
-		_ = f.Value.Set(strings.Join(s, ","))
+		_ = f.Value.Set(strings.Join(safe, ","))
 	})
 }
 
