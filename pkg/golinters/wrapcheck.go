92fda268b20f9cc3c1f8d860d0a7e8f676e2e987
Tom Arrell <tom.arrell@gmail.com>
2021-05-01 00:13:51 +0200

    Update Wrapcheck to v2, add configuration (#1947)

--- pkg/golinters/wrapcheck.go
+++ pkg/golinters/wrapcheck.go
@@ -1,19 +1,29 @@
 package golinters
 
 import (
-	"github.com/tomarrell/wrapcheck/wrapcheck"
+	"github.com/tomarrell/wrapcheck/v2/wrapcheck"
 	"golang.org/x/tools/go/analysis"
 
+	"github.com/golangci/golangci-lint/pkg/config"
 	"github.com/golangci/golangci-lint/pkg/golinters/goanalysis"
 )
 
 const wrapcheckName = "wrapcheck"
 
-func NewWrapcheck() *goanalysis.Linter {
+func NewWrapcheck(settings *config.WrapcheckSettings) *goanalysis.Linter {
+	cfg := wrapcheck.NewDefaultConfig()
+	if settings != nil {
+		if len(settings.IgnoreSigs) != 0 {
+			cfg.IgnoreSigs = settings.IgnoreSigs
+		}
+	}
+
+	a := wrapcheck.NewAnalyzer(cfg)
+
 	return goanalysis.NewLinter(
 		wrapcheckName,
-		wrapcheck.Analyzer.Doc,
-		[]*analysis.Analyzer{wrapcheck.Analyzer},
+		a.Doc,
+		[]*analysis.Analyzer{a},
 		nil,
 	).WithLoadMode(goanalysis.LoadModeTypesInfo)
 }
