286471ff7f812476aff2c3c89df46be779478b33
Oleg Butuzov <butuzov@made.ua>
2021-09-07 05:57:00 +0300

    test: added stdlib test

--- test/testdata/ireturn_reject_stdlib.go
+++ test/testdata/ireturn_reject_stdlib.go
@@ -7,7 +7,22 @@ import (
 	"io"
 )
 
-func NewAllowDoer() io.Writer {
+func NewWriter() io.Writer { // ERROR `NewWriter returns interface \(io.Writer\)`
 	var buf bytes.Buffer
 	return &buf
+}
+
+func TestError() error {
+	return nil
+}
+
+type Foo interface {
+	Foo()
+}
+type foo int
+
+func (f foo) Foo() {}
+
+func NewFoo() Foo {
+	return foo(1)
 }
