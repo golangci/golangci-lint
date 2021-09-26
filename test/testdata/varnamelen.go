f96a1108f8eaa94c5ef2804d681449d39a4615fc
Fernandez Ludovic <ldez@users.noreply.github.com>
2021-09-26 20:01:33 +0200

    review: use std import in the linter test.

--- test/testdata/varnamelen.go
+++ test/testdata/varnamelen.go
@@ -1,8 +1,10 @@
 //args: -Evarnamelen
 package testdata
 
+import "math"
+
 func varnamelen() {
-	x := 1 // ERROR "variable name 'x' is too short for the scope of its usage"
+	x := math.MinInt8 // ERROR "variable name 'x' is too short for the scope of its usage"
 	x++
 	x++
 	x++
