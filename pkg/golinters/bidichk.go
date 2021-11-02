bb60b6726db80e7db5eead46c5ac00675c7c6fa9
Fernandez Ludovic <lfernandez.dev@gmail.com>
2021-11-02 09:41:30 +0100

    fix: imports

--- pkg/golinters/bidichk.go
+++ pkg/golinters/bidichk.go
@@ -2,8 +2,9 @@ package golinters
 
 import (
 	"github.com/breml/bidichk/pkg/bidichk"
-	"github.com/golangci/golangci-lint/pkg/golinters/goanalysis"
 	"golang.org/x/tools/go/analysis"
+
+	"github.com/golangci/golangci-lint/pkg/golinters/goanalysis"
 )
 
 func NewBiDiChkFuncName() *goanalysis.Linter {
