0a6c8d1fdac6982039e18b1225b14b364992b510
Lucas Bremgartner <lucas@bremis.ch>
2021-11-23 21:38:17 +0100

    Fix order of imports

--- pkg/golinters/errchkjson.go
+++ pkg/golinters/errchkjson.go
@@ -1,8 +1,8 @@
 package golinters
 
 import (
-	"golang.org/x/tools/go/analysis"
 	"github.com/breml/errchkjson"
+	"golang.org/x/tools/go/analysis"
 
 	"github.com/golangci/golangci-lint/pkg/config"
 	"github.com/golangci/golangci-lint/pkg/golinters/goanalysis"
