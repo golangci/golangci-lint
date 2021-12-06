0a861342cfa433379583c6854550ccea22bafb19
connorszczepaniak-wk <connor.szczepaniak@workiva.com>
2021-12-06 16:34:27 -0600

    add more to test

--- test/testdata/multiimport.go
+++ test/testdata/multiimport.go
@@ -4,9 +4,11 @@ package testdata
 import (
 	"fmt"      // ERROR "import appears multiple times under different aliases"
 	blah "fmt" // ERROR "import appears multiple times under different aliases"
+	blah2 "log"
 )
 
 func MultiImportIssue() {
 	fmt.Println(`a`)
 	blah.Println(`a`)
+	blah2.Print(`hi`)
 }
