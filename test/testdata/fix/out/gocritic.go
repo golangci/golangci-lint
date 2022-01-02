a473907d0d922eeed734abd20277e30bb5480ffb
Denis <d.limarev@corp.mail.ru>
2022-01-02 20:59:29 +0300

    add test cases

--- test/testdata/fix/out/gocritic.go
+++ test/testdata/fix/out/gocritic.go
@@ -1,7 +1,11 @@
 //args: -Egocritic
 //config: linters-settings.gocritic.enabled-checks=ruleguard
-//config: linters-settings.gocritic.settings.ruleguard.rules=ruleguard/rangeExprCopy.go
+//config: linters-settings.gocritic.settings.ruleguard.rules=ruleguard/rangeExprCopy.go,ruleguard/strings_simplify.go
 package p
+
+import (
+	"strings"
+)
 
 func gocritic() {
 	var xs [2048]byte
@@ -9,5 +13,10 @@ func gocritic() {
 	// xs -> &xs
 	for _, x := range &xs {
 		print(x)
+	}
+
+	// strings.Count("foo", "bar") == 0 -> !strings.Contains("foo", "bar")
+	if !strings.Contains("foo", "bar") {
+		print("qu")
 	}
 }
