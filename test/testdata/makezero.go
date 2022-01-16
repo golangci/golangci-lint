6e2e51d859f1a02a22bf0b8fee976fc5e38c8d4f
Andrew Shannon Brown <ashanbrown@users.noreply.github.com>
2022-01-16 13:48:49 -0800

    Bump makezero to v1.1.0 (#2490)

--- test/testdata/makezero.go
+++ test/testdata/makezero.go
@@ -8,6 +8,12 @@ func Makezero() []int {
 	return append(x, 1) // ERROR "append to slice `x` with non-zero initialized length"
 }
 
+func MakezeroMultiple() []int {
+	x, y := make([]int, math.MaxInt8), make([]int, math.MaxInt8)
+	return append(x, // ERROR "append to slice `x` with non-zero initialized length"
+		append(y, 1)...) // ERROR "append to slice `y` with non-zero initialized length"
+}
+
 func MakezeroNolint() []int {
 	x := make([]int, math.MaxInt8)
 	return append(x, 1) //nolint:makezero // ok that we're appending to an uninitialized slice
