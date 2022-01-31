deb352c60dce0b9b9f8f6c43eaee06d4df1d13b6
Norman Gehrsitz <norman.gehrsitz@gmx.de>
2022-01-31 19:21:09 +0100

    Run gci as part of the CI
    Signed-off-by: Norman Gehrsitz <norman.gehrsitz@gmx.de>

--- pkg/golinters/ireturn.go
+++ pkg/golinters/ireturn.go
@@ -3,11 +3,11 @@ package golinters
 import (
 	"strings"
 
-	"github.com/golangci/golangci-lint/pkg/config"
-	"github.com/golangci/golangci-lint/pkg/golinters/goanalysis"
-
 	"github.com/butuzov/ireturn/analyzer"
 	"golang.org/x/tools/go/analysis"
+
+	"github.com/golangci/golangci-lint/pkg/config"
+	"github.com/golangci/golangci-lint/pkg/golinters/goanalysis"
 )
 
 func NewIreturn(settings *config.IreturnSettings) *goanalysis.Linter {
