f5b1fec1170b1781cf9b935289cb6733f204bdc1
Fernandez Ludovic <ldez@users.noreply.github.com>
2022-03-27 14:02:19 +0200

    update tests

--- test/testdata/errchkjson_check_error_free_encoding.go
+++ test/testdata/errchkjson_check_error_free_encoding.go
@@ -34,9 +34,9 @@ func JSONMarshalSafeTypes() {
 	_ = err
 
 	enc := json.NewEncoder(ioutil.Discard)
-	_ = enc.Encode(nil)   // nil is safe
-	enc.Encode(nil)       // nil is safe
-	err = enc.Encode(nil) // ERROR "Error return value of `\\([*]encoding/json.Encoder\\).Encode` is checked but passed argument is safe"
+	_ = enc.Encode(nil) // ERROR "Error return value of `\\([*]encoding/json.Encoder\\).Encode` is not checked"
+	enc.Encode(nil)     // ERROR "Error return value of `\\([*]encoding/json.Encoder\\).Encode` is not checked"
+	err = enc.Encode(nil)
 	_ = err
 
 	var b bool
