eef9f55a7116e755b584192d7a9c96ff4d31de51
Fernandez Ludovic <ldez@users.noreply.github.com>
2022-03-29 04:49:24 +0200

    review: fix tests

--- pkg/golinters/execinquery.go
+++ pkg/golinters/execinquery.go
@@ -1,17 +1,19 @@
 package golinters
 
 import (
+	"github.com/golangci/golangci-lint/pkg/golinters/goanalysis"
 	"github.com/lufeee/execinquery"
 	"golang.org/x/tools/go/analysis"
-
-	"github.com/golangci/golangci-lint/pkg/golinters/goanalysis"
 )
 
 func NewExecInQuery() *goanalysis.Linter {
+	const linterName = "execinquery"
+
 	a := execinquery.Analyzer
+	a.Name = linterName // TODO the name must change inside the linter.
 
 	return goanalysis.NewLinter(
-		"execinquery",
+		linterName,
 		a.Doc,
 		[]*analysis.Analyzer{a},
 		nil,
--- test/testdata/execinquery.go
+++ test/testdata/execinquery.go
@@ -1,34 +1,30 @@
-//args: -Eexecinquery
+// args: -Eexecinquery
 package testdata
 
 import (
+	"context"
 	"database/sql"
-
-	"golang.org/x/net/context"
 )
 
-func execinquery() {
-	db, _ := sql.Open("mysql", "test:test@tcp(test:3306)/test")
-
+func execInQuery(db *sql.DB) {
 	test := "a"
 
-	_, err := db.Query("Update * FROM hoge where id = ?", test) // ERROR "Query not `SELECT` query"
+	_, err := db.Query("Update * FROM hoge where id = ?", test) // ERROR "It's better to use Execute method instead of Query method to execute `UPDATE` query"
 	if err != nil {
 		return
 	}
 
-	db.QueryRow("Update * FROM hoge where id = ?", test) // ERROR "Query not `SELECT` query"
+	db.QueryRow("Update * FROM hoge where id = ?", test) // ERROR "It's better to use Execute method instead of QueryRow method to execute `UPDATE` query"
 	if err != nil {
 		return
 	}
 
 	ctx := context.Background()
 
-	_, err = db.QueryContext(ctx, "Update * FROM hoge where id = ?", test) // ERROR "Query not `SELECT` query"
+	_, err = db.QueryContext(ctx, "Update * FROM hoge where id = ?", test) // ERROR "It's better to use Execute method instead of QueryContext method to execute `UPDATE` query "
 	if err != nil {
 		return
 	}
 
-	db.QueryRowContext(ctx, "Update * FROM hoge where id = ?", test) // ERROR "Query not `SELECT` query"
-
+	db.QueryRowContext(ctx, "Update * FROM hoge where id = ?", test) // ERROR "It's better to use Execute method instead of QueryRowContext method to execute `UPDATE` query"
 }
