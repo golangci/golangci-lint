558147aac6eb10303c14728974a2b1efe4e2f50f
renai <>
2022-03-31 16:09:12 +0800

    fix install

--- install.sh
+++ install.sh
@@ -366,7 +366,7 @@ End of functions from https://github.com/client9/shlib
 EOF
 
 PROJECT_NAME="golangci-lint"
-OWNER=golangci
+OWNER=LilithGames
 REPO="golangci-lint"
 BINARY=golangci-lint
 FORMAT=tar.gz
