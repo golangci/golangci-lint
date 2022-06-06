a7bfc6674008abda1fcc1fc73385cddeed5539f5
Ludovic Fernandez <ldez@users.noreply.github.com>
2022-06-06 13:20:19 +0200

    docs: add thanks page (#2893)

--- scripts/expand_website_templates/main.go
+++ scripts/expand_website_templates/main.go
@@ -313,32 +313,77 @@ func spanWithID(id, title, icon string) string {
 	return fmt.Sprintf(`<span id=%q title=%q>%s</span>`, id, title, icon)
 }
 
+type authorDetails struct {
+	Linters []string
+	Profile string
+	Avatar  string
+}
+
 func getThanksList() string {
-	var lines []string
-	addedAuthors := map[string]bool{}
+	addedAuthors := map[string]*authorDetails{}
+
 	for _, lc := range lintersdb.NewManager(nil, nil).GetAllSupportedLinterConfigs() {
 		if lc.OriginalURL == "" {
 			continue
 		}
 
-		const githubPrefix = "https://github.com/"
-		if !strings.HasPrefix(lc.OriginalURL, githubPrefix) {
-			continue
+		linterURL := lc.OriginalURL
+		if lc.Name() == "staticcheck" {
+			linterURL = "https://github.com/dominikh/go-tools"
 		}
 
-		githubSuffix := strings.TrimPrefix(lc.OriginalURL, githubPrefix)
-		githubAuthor := strings.Split(githubSuffix, "/")[0]
-		if addedAuthors[githubAuthor] {
+		if author := extractAuthor(linterURL, "https://github.com/"); author != "" && author != "golangci" {
+			if _, ok := addedAuthors[author]; ok {
+				addedAuthors[author].Linters = append(addedAuthors[author].Linters, lc.Name())
+			} else {
+				addedAuthors[author] = &authorDetails{
+					Linters: []string{lc.Name()},
+					Profile: fmt.Sprintf("[%[1]s](https://github.com/sponsors/%[1]s)", author),
+					Avatar:  fmt.Sprintf(`<img src="https://github.com/%[1]s.png" alt="%[1]s" style="max-width: 100%%;" width="20px;" />`, author),
+				}
+			}
+		} else if author := extractAuthor(linterURL, "https://gitlab.com/"); author != "" {
+			if _, ok := addedAuthors[author]; ok {
+				addedAuthors[author].Linters = append(addedAuthors[author].Linters, lc.Name())
+			} else {
+				addedAuthors[author] = &authorDetails{
+					Linters: []string{lc.Name()},
+					Profile: fmt.Sprintf("[%[1]s](https://gitlab.com/%[1]s)", author),
+				}
+			}
+		} else {
 			continue
 		}
-		addedAuthors[githubAuthor] = true
+	}
 
-		line := fmt.Sprintf("- [%s](https://github.com/%s)",
-			githubAuthor, githubAuthor)
-		lines = append(lines, line)
+	var authors []string
+	for author := range addedAuthors {
+		authors = append(authors, author)
+	}
+
+	sort.Slice(authors, func(i, j int) bool {
+		return strings.ToLower(authors[i]) < strings.ToLower(authors[j])
+	})
+
+	lines := []string{
+		"|Author|Linter(s)|",
+		"|---|---|",
+	}
+
+	for _, author := range authors {
+		lines = append(lines, fmt.Sprintf("|%s %s|%s|",
+			addedAuthors[author].Avatar, addedAuthors[author].Profile, strings.Join(addedAuthors[author].Linters, ", ")))
 	}
 
 	return strings.Join(lines, "\n")
+}
+
+func extractAuthor(originalURL, prefix string) string {
+	if !strings.HasPrefix(originalURL, prefix) {
+		return ""
+	}
+
+	return strings.SplitN(strings.TrimPrefix(originalURL, prefix), "/", 2)[0]
 }
 
 type SettingSnippets struct {
