2c1e28e697c02c1778054fb459fb1a28d846938c
Fernandez Ludovic <ldez@users.noreply.github.com>
2022-07-14 03:53:51 +0200

    review

---
+++ pkg/golinters/goanalysis/runner_loadingpackage_ti.go
@@ -0,0 +1,21 @@
+//go:build go1.18
+// +build go1.18
+
+package goanalysis
+
+import (
+	"go/ast"
+	"go/types"
+)
+
+func newTypesInfo() *types.Info {
+	return &types.Info{
+		Types:      make(map[ast.Expr]types.TypeAndValue),
+		Instances:  make(map[*ast.Ident]types.Instance),
+		Defs:       make(map[*ast.Ident]types.Object),
+		Uses:       make(map[*ast.Ident]types.Object),
+		Implicits:  make(map[ast.Node]types.Object),
+		Scopes:     make(map[ast.Node]*types.Scope),
+		Selections: make(map[*ast.SelectorExpr]*types.Selection),
+	}
+}
