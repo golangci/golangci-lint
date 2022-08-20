6e756cb7b51d399a5a6cf8b3a49030db910d6305
Fernandez Ludovic <ldez@users.noreply.github.com>
2022-08-20 18:41:02 +0200

    improve output tests

--- test/output_test.go
+++ test/output_test.go
@@ -12,8 +12,11 @@ import (
 	"github.com/golangci/golangci-lint/test/testshared"
 )
 
+//nolint:misspell,lll
+const expectedJSONOutput = `{"Issues":[{"FromLinter":"misspell","Text":"` + "`" + `occured` + "`" + ` is a misspelling of ` + "`" + `occurred` + "`" + `","Severity":"","SourceLines":["\t// comment with incorrect spelling: occured // want \"` + "`" + `occured` + "`" + ` is a misspelling of ` + "`" + `occurred` + "`" + `\""],"Replacement":{"NeedOnlyDelete":false,"NewLines":null,"Inline":{"StartCol":37,"Length":7,"NewString":"occurred"}},"Pos":{"Filename":"testdata/misspell.go","Offset":0,"Line":6,"Column":38},"ExpectNoLint":false,"ExpectedNoLintLinter":""}],"Report":{"Linters":[{"Name":"asasalint"},{"Name":"asciicheck"},{"Name":"bidichk"},{"Name":"bodyclose"},{"Name":"containedctx"},{"Name":"contextcheck"},{"Name":"cyclop"},{"Name":"decorder"},{"Name":"deadcode","EnabledByDefault":true},{"Name":"depguard"},{"Name":"dogsled"},{"Name":"dupl"},{"Name":"durationcheck"},{"Name":"errcheck","EnabledByDefault":true},{"Name":"errchkjson"},{"Name":"errname"},{"Name":"errorlint"},{"Name":"execinquery"},{"Name":"exhaustive"},{"Name":"exhaustivestruct"},{"Name":"exhaustruct"},{"Name":"exportloopref"},{"Name":"forbidigo"},{"Name":"forcetypeassert"},{"Name":"funlen"},{"Name":"gci"},{"Name":"gochecknoglobals"},{"Name":"gochecknoinits"},{"Name":"gocognit"},{"Name":"goconst"},{"Name":"gocritic"},{"Name":"gocyclo"},{"Name":"godot"},{"Name":"godox"},{"Name":"goerr113"},{"Name":"gofmt"},{"Name":"gofumpt"},{"Name":"goheader"},{"Name":"goimports"},{"Name":"golint"},{"Name":"gomnd"},{"Name":"gomoddirectives"},{"Name":"gomodguard"},{"Name":"goprintffuncname"},{"Name":"gosec"},{"Name":"gosimple","EnabledByDefault":true},{"Name":"govet","EnabledByDefault":true},{"Name":"grouper"},{"Name":"ifshort"},{"Name":"importas"},{"Name":"ineffassign","EnabledByDefault":true},{"Name":"interfacer"},{"Name":"ireturn"},{"Name":"lll"},{"Name":"maintidx"},{"Name":"makezero"},{"Name":"maligned"},{"Name":"misspell","Enabled":true},{"Name":"nakedret"},{"Name":"nestif"},{"Name":"nilerr"},{"Name":"nilnil"},{"Name":"nlreturn"},{"Name":"noctx"},{"Name":"nonamedreturns"},{"Name":"nosnakecase"},{"Name":"nosprintfhostport"},{"Name":"paralleltest"},{"Name":"prealloc"},{"Name":"predeclared"},{"Name":"promlinter"},{"Name":"revive"},{"Name":"rowserrcheck"},{"Name":"scopelint"},{"Name":"sqlclosecheck"},{"Name":"staticcheck","EnabledByDefault":true},{"Name":"structcheck"},{"Name":"stylecheck"},{"Name":"tagliatelle"},{"Name":"tenv"},{"Name":"testpackage"},{"Name":"thelper"},{"Name":"tparallel"},{"Name":"typecheck","EnabledByDefault":true},{"Name":"unconvert"},{"Name":"unparam"},{"Name":"unused","EnabledByDefault":true},{"Name":"usestdlibvars"},{"Name":"varcheck","EnabledByDefault":true},{"Name":"varnamelen"},{"Name":"wastedassign"},{"Name":"whitespace"},{"Name":"wrapcheck"},{"Name":"wsl"},{"Name":"nolintlint"}]}}`
+
 func TestOutput_Stderr(t *testing.T) {
-	sourcePath := filepath.Join(testdataDir, "gci.go")
+	sourcePath := filepath.Join(testdataDir, "misspell.go")
 	fmt.Println(filepath.Abs(sourcePath))
 
 	testshared.NewRunnerBuilder(t).
@@ -28,14 +31,15 @@ func TestOutput_Stderr(t *testing.T) {
 		Runner().
 		Install().
 		Run().
-		ExpectHasIssue("testdata/gci.go:8: File is not `gci`-ed").
-		ExpectOutputContains(`"Issues":[`)
+		//nolint:misspell
+		ExpectHasIssue("testdata/misspell.go:6:38: `occured` is a misspelling of `occurred`").
+		ExpectOutputContains(expectedJSONOutput)
 }
 
 func TestOutput_File(t *testing.T) {
 	resultPath := path.Join(t.TempDir(), "golangci_lint_test_result")
 
-	sourcePath := filepath.Join(testdataDir, "gci.go")
+	sourcePath := filepath.Join(testdataDir, "misspell.go")
 
 	testshared.NewRunnerBuilder(t).
 		WithArgs(
@@ -49,16 +53,16 @@ func TestOutput_File(t *testing.T) {
 		Runner().
 		Install().
 		Run().
-		ExpectHasIssue("testdata/gci.go:8: File is not `gci`-ed").
-		ExpectOutputNotContains(`"Issues":[`)
+		//nolint:misspell
+		ExpectHasIssue("testdata/misspell.go:6:38: `occured` is a misspelling of `occurred`")
 
 	b, err := os.ReadFile(resultPath)
 	require.NoError(t, err)
-	require.Contains(t, string(b), `"Issues":[`)
+	require.Contains(t, string(b), expectedJSONOutput)
 }
 
 func TestOutput_Multiple(t *testing.T) {
-	sourcePath := filepath.Join(testdataDir, "gci.go")
+	sourcePath := filepath.Join(testdataDir, "misspell.go")
 
 	testshared.NewRunnerBuilder(t).
 		WithArgs(
@@ -72,6 +76,7 @@ func TestOutput_Multiple(t *testing.T) {
 		Runner().
 		Install().
 		Run().
-		ExpectHasIssue("testdata/gci.go:8: File is not `gci`-ed").
-		ExpectOutputContains(`"Issues":[`)
+		//nolint:misspell
+		ExpectHasIssue("testdata/misspell.go:6:38: `occured` is a misspelling of `occurred`").
+		ExpectOutputContains(expectedJSONOutput)
 }
