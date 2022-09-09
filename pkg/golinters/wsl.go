ad084e7e3473f9c593e8ad124bcb1d2456746fc8
xobotyi <xog3@yandex.ru>
2022-09-09 15:57:40 +0300

    [wsl]: support all configs and update docs
    Now options docs aligned with wsl's and settings supports all of wsl
    configs.

--- pkg/golinters/wsl.go
+++ pkg/golinters/wsl.go
@@ -19,30 +19,26 @@ func NewWSL(settings *config.WSLSettings) *goanalysis.Linter {
 	var mu sync.Mutex
 	var resIssues []goanalysis.Issue
 
-	conf := &wsl.Configuration{
-		AllowCuddleWithCalls: []string{"Lock", "RLock"},
-		AllowCuddleWithRHS:   []string{"Unlock", "RUnlock"},
-		ErrorVariableNames:   []string{"err"},
-	}
+	conf := wsl.DefaultConfig()
 
 	if settings != nil {
 		conf.StrictAppend = settings.StrictAppend
 		conf.AllowAssignAndCallCuddle = settings.AllowAssignAndCallCuddle
 		conf.AllowAssignAndAnythingCuddle = settings.AllowAssignAndAnythingCuddle
 		conf.AllowMultiLineAssignCuddle = settings.AllowMultiLineAssignCuddle
-		conf.AllowCuddleDeclaration = settings.AllowCuddleDeclaration
+		conf.ForceCaseTrailingWhitespaceLimit = settings.ForceCaseTrailingWhitespaceLimit
 		conf.AllowTrailingComment = settings.AllowTrailingComment
 		conf.AllowSeparatedLeadingComment = settings.AllowSeparatedLeadingComment
-		conf.ForceCuddleErrCheckAndAssign = settings.ForceCuddleErrCheckAndAssign
-		conf.ForceCaseTrailingWhitespaceLimit = settings.ForceCaseTrailingWhitespaceLimit
-		conf.ForceExclusiveShortDeclarations = settings.ForceExclusiveShortDeclarations
+		conf.AllowCuddleDeclaration = settings.AllowCuddleDeclaration
+		conf.AllowCuddleWithCalls = settings.AllowCuddleWithCalls
+		conf.AllowCuddleWithRHS = settings.AllowCuddleWithRHS
 	}
 
 	analyzer := &analysis.Analyzer{
 		Name: goanalysis.TheOnlyAnalyzerName,
 		Doc:  goanalysis.TheOnlyanalyzerDoc,
 		Run: func(pass *analysis.Pass) (interface{}, error) {
-			issues := runWSL(pass, conf)
+			issues := runWSL(pass, &conf)
 
 			if len(issues) == 0 {
 				return nil, nil
