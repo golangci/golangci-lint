f246acfb770543300513f89b2896a83fa7f08bfb
Fernandez Ludovic <ldez@users.noreply.github.com>
2022-09-11 13:34:43 +0200

    fix the fixer

--- pkg/result/processors/fixer.go
+++ pkg/result/processors/fixer.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/pkg/errors"
 
+	"github.com/golangci/golangci-lint/internal/robustio"
 	"github.com/golangci/golangci-lint/pkg/config"
 	"github.com/golangci/golangci-lint/pkg/fsutils"
 	"github.com/golangci/golangci-lint/pkg/logutils"
@@ -104,13 +105,13 @@ func (f Fixer) fixIssuesInFile(filePath string, issues []result.Issue) error {
 
 	if err = f.writeFixedFile(origFileLines, issues, tmpOutFile); err != nil {
 		tmpOutFile.Close()
-		os.Remove(tmpOutFile.Name())
+		_ = robustio.RemoveAll(tmpOutFile.Name())
 		return err
 	}
 
 	tmpOutFile.Close()
-	if err = os.Rename(tmpOutFile.Name(), filePath); err != nil {
-		os.Remove(tmpOutFile.Name())
+	if err = robustio.Rename(tmpOutFile.Name(), filePath); err != nil {
+		_ = robustio.RemoveAll(tmpOutFile.Name())
 		return errors.Wrapf(err, "failed to rename %s -> %s", tmpOutFile.Name(), filePath)
 	}
 
