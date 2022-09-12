8f00a10ad7df2f5ea224d9285e8d5b1906d7222c
Ludovic Fernandez <ldez@users.noreply.github.com>
2022-09-12 09:01:27 +0200

    dev: improve tests on Windows (#3211)

--- test/testdata/depguard_ignore_file_rules.go
+++ test/testdata/depguard_ignore_file_rules.go
@@ -1,11 +1,9 @@
-//go:build !windows
-
 //golangcitest:args -Edepguard
 //golangcitest:config_path testdata/configs/depguard_ignore_file_rules.yml
 //golangcitest:expected_exitcode 0
 package testdata
 
-// NOTE - No lint errors becuase this file is ignored
+// NOTE - No lint errors because this file is ignored
 import (
 	"compress/gzip"
 	"log"
