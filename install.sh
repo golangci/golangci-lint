d57156ec228b712ccd429367482771179e3fa050
Mark Fine <mark.fine@gmail.com>
2023-01-27 17:17:38 -0800

    chore: fix install.sh to point at releases API (#3510)

--- install.sh
+++ install.sh
@@ -317,11 +317,14 @@ http_copy() {
 github_release() {
   owner_repo=$1
   version=$2
-  test -z "$version" && version="latest"
-  giturl="https://github.com/${owner_repo}/releases/${version}"
+  if [ -z "$version" ]; then
+      giturl="https://api.github.com/repos/${owner_repo}/releases/latest"
+  else
+      giturl="https://api.github.com/repos/${owner_repo}/releases/tags/${version}"
+  fi
   json=$(http_copy "$giturl" "Accept:application/json")
   test -z "$json" && return 1
-  version=$(echo "$json" | tr -s '\n' ' ' | sed 's/.*"tag_name":"//' | sed 's/".*//')
+  version=$(echo "$json" | tr -s '\n' ' ' | sed 's/.*"tag_name": "//' | sed 's/".*//')
   test -z "$version" && return 1
   echo "$version"
 }
