060f13545c497550013b962bfa251e724ca308a9
Mitsuo Heijo <mitsuo.heijo@gmail.com>
2023-02-17 21:29:16 +0900

    Enable timeformat by default

--- pkg/golinters/govet.go
+++ pkg/golinters/govet.go
@@ -89,7 +89,6 @@ var (
 		unusedwrite.Analyzer,
 	}
 
-	// https://github.com/golang/go/blob/879db69ce2de814bc3203c39b45617ba51cc5366/src/cmd/vet/main.go#L40-L68
 	defaultAnalyzers = []*analysis.Analyzer{
 		asmdecl.Analyzer,
 		assign.Analyzer,
@@ -114,6 +113,7 @@ var (
 		structtag.Analyzer,
 		testinggoroutine.Analyzer,
 		tests.Analyzer,
+		timeformat.Analyzer,
 		unmarshal.Analyzer,
 		unreachable.Analyzer,
 		unsafeptr.Analyzer,
