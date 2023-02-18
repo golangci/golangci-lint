395eef33b7e43e01bd8edebda3ce7ffdd8811ecb
Gabor Javorszky <gabor@suborbital.dev>
2023-02-18 20:17:26 +0000

    Rework imports to goimports is happy

--- pkg/golinters/responsewriterlint.go
+++ pkg/golinters/responsewriterlint.go
@@ -1,10 +1,11 @@
 package golinters
 
 import (
-	"github.com/golangci/golangci-lint/pkg/config"
-	"github.com/golangci/golangci-lint/pkg/golinters/goanalysis"
 	"github.com/javorszky/go-responsewriter-lint/pkg/analyzer"
 	"golang.org/x/tools/go/analysis"
+
+	"github.com/golangci/golangci-lint/pkg/config"
+	"github.com/golangci/golangci-lint/pkg/golinters/goanalysis"
 )
 
 func NewResponseWriterLint(_ *config.ReassignSettings) *goanalysis.Linter {
