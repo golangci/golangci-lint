dafae80ec4578f1d21e4b9e8e50d35798a2daf67
Fernandez Ludovic <ldez@users.noreply.github.com>
2023-02-22 15:15:44 +0100

    dev: add debug version

--- pkg/commands/version.go
+++ pkg/commands/version.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"runtime/debug"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -12,6 +13,11 @@ import (
 
 	"github.com/golangci/golangci-lint/pkg/config"
 )
+
+type versionInfo struct {
+	Info      BuildInfo
+	BuildInfo *debug.BuildInfo
+}
 
 func (e *Executor) initVersionConfiguration(cmd *cobra.Command) {
 	fs := cmd.Flags()
@@ -23,6 +29,7 @@ func initVersionFlagSet(fs *pflag.FlagSet, cfg *config.Config) {
 	// Version config
 	vc := &cfg.Version
 	fs.StringVar(&vc.Format, "format", "", wh("The version's format can be: 'short', 'json'"))
+	fs.BoolVar(&vc.Debug, "debug", false, wh("Add build information"))
 }
 
 func (e *Executor) initVersion() {
@@ -32,6 +39,25 @@ func (e *Executor) initVersion() {
 		Args:              cobra.NoArgs,
 		ValidArgsFunction: cobra.NoFileCompletions,
 		RunE: func(cmd *cobra.Command, _ []string) error {
+			if e.cfg.Version.Debug {
+				info, ok := debug.ReadBuildInfo()
+				if !ok {
+					return nil
+				}
+
+				switch strings.ToLower(e.cfg.Version.Format) {
+				case "json":
+					return json.NewEncoder(os.Stdout).Encode(versionInfo{
+						Info:      e.buildInfo,
+						BuildInfo: info,
+					})
+
+				default:
+					fmt.Println(info.String())
+					return printVersion(os.Stdout, e.buildInfo)
+				}
+			}
+
 			switch strings.ToLower(e.cfg.Version.Format) {
 			case "short":
 				fmt.Println(e.buildInfo.Version)
