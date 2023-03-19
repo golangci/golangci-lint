dcff4c8fdd944417e1aedc3b6a4776e00a46a741
fourmeepo <fourmeepo@outlook.com>
2023-03-19 22:25:54 +0800

    bump tagalign version to v1.1.5

--- test/testdata/tagalign.go
+++ test/testdata/tagalign.go
@@ -7,3 +7,9 @@ type TagAlignSortExample struct {
 	Bar    int `validate:"required"  yaml:"foo" xml:"bar" binding:"required" json:"bar,omitempty" gorm:"column:bar" zip:"bar" `  // want `tag is not aligned, should be: json:"bar,omitempty" yaml:"foo" xml:"bar" binding:"required" gorm:"column:bar" validate:"required" zip:"bar"`
 	FooBar int `gorm:"column:bar" validate:"required"   xml:"bar" binding:"required" json:"bar,omitempty"  zip:"bar" yaml:"foo"` // want `tag is not aligned, should be: json:"bar,omitempty" yaml:"foo" xml:"bar" binding:"required" gorm:"column:bar" validate:"required" zip:"bar"`
 }
+
+type TagAlignSortExample2 struct {
+	Foo int ` xml:"baz"  json:"foo,omitempty" yaml:"bar"  zip:"foo"  binding:"required" gorm:"column:foo"  validate:"required"` // want `tag is not aligned , should be: json:"foo,omitempty" yaml:"bar" xml:"baz" binding:"required" gorm:"column:foo" validate:"required" zip:"foo"`
+
+	Bar int `validate:"required" gorm:"column:bar"  yaml:"foo" xml:"bar" binding:"required" json:"bar,omitempty" zip:"bar" ` // want `tag is not aligned , should be: json:"bar,omitempty" yaml:"foo" xml:"bar" binding:"required" gorm:"column:bar" validate:"required" zip:"bar"`
+}
