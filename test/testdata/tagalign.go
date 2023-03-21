732f36c229b5c11a9ddffc8f8c64f656afdeadb9
fourmeepo <fourmeepo@outlook.com>
2023-03-21 23:31:19 +0800

    optimize content of want

--- test/testdata/tagalign.go
+++ test/testdata/tagalign.go
@@ -3,9 +3,9 @@
 package testdata
 
 type TagAlignSortExample struct {
-	Foo    int `json:"foo,omitempty" yaml:"bar" xml:"baz" binding:"required" gorm:"column:foo" zip:"foo" validate:"required"`    // want `tag is not aligned, should be: json:"foo,omitempty" yaml:"bar" xml:"baz" binding:"required" gorm:"column:foo" validate:"required" zip:"foo"`
-	Bar    int `validate:"required"  yaml:"foo" xml:"bar" binding:"required" json:"bar,omitempty" gorm:"column:bar" zip:"bar" `  // want `tag is not aligned, should be: json:"bar,omitempty" yaml:"foo" xml:"bar" binding:"required" gorm:"column:bar" validate:"required" zip:"bar"`
-	FooBar int `gorm:"column:bar" validate:"required"   xml:"bar" binding:"required" json:"bar,omitempty"  zip:"bar" yaml:"foo"` // want `tag is not aligned, should be: json:"bar,omitempty" yaml:"foo" xml:"bar" binding:"required" gorm:"column:bar" validate:"required" zip:"bar"`
+	Foo    int `json:"foo,omitempty" yaml:"bar" xml:"baz" binding:"required" gorm:"column:foo" zip:"foo" validate:"required"`    // want `json:"foo,omitempty" yaml:"bar" xml:"baz" binding:"required" gorm:"column:foo" validate:"required" zip:"foo"`
+	Bar    int `validate:"required"  yaml:"foo" xml:"bar" binding:"required" json:"bar,omitempty" gorm:"column:bar" zip:"bar" `  // want `json:"bar,omitempty" yaml:"foo" xml:"bar" binding:"required" gorm:"column:bar" validate:"required" zip:"bar"`
+	FooBar int `gorm:"column:bar" validate:"required"   xml:"bar" binding:"required" json:"bar,omitempty"  zip:"bar" yaml:"foo"` // want `json:"bar,omitempty" yaml:"foo" xml:"bar" binding:"required" gorm:"column:bar" validate:"required" zip:"bar"`
 }
 
 type TagAlignSortExample2 struct {
--- test/testdata/tagalign_no_sort.go
+++ test/testdata/tagalign_no_sort.go
@@ -2,27 +2,27 @@
 package testdata
 
 type TagAlignSortExampleNoSortExample struct {
-	Foo    int    `json:"foo"        validate:"required"`                    // want `tag is not aligned, should be: json:"foo"`
+	Foo    int    `json:"foo"        validate:"required"`                    // want `json:"foo"`
 	Bar    string `json:"___bar___,omitempty"  validate:"required"`          // want `json:"___bar___,omitempty" validate:"required"`
-	FooFoo int8   `json:"foo_foo"    validate:"required"      yaml:"fooFoo"` // want `tag is not aligned, should be: json:"foo_foo"`
-	BarBar int    `json:"bar_bar"         validate:"required"`               // want `tag is not aligned, should be: json:"bar_bar"`
+	FooFoo int8   `json:"foo_foo"    validate:"required"      yaml:"fooFoo"` // want `json:"foo_foo"`
+	BarBar int    `json:"bar_bar"         validate:"required"`               // want `json:"bar_bar"`
 	FooBar struct {
-		Foo    int    `json:"foo"    yaml:"foo"     validate:"required"` // want `tag is not aligned, should be: json:"foo"    yaml:"foo"          validate:"required"`
-		Bar222 string `json:"bar222"   validate:"required"  yaml:"bar"`  // want `tag is not aligned, should be: json:"bar222" validate:"required" yaml:"bar"`
+		Foo    int    `json:"foo"    yaml:"foo"     validate:"required"` // want `json:"foo"    yaml:"foo"          validate:"required"`
+		Bar222 string `json:"bar222"   validate:"required"  yaml:"bar"`  // want `json:"bar222" validate:"required" yaml:"bar"`
 	} `json:"foo_bar" validate:"required"`
 	FooFooFoo struct {
 		BarBarBar struct {
 			BarBarBarBar    string `json:"bar_bar_bar_bar" validate:"required"`                // want `json:"bar_bar_bar_bar"     validate:"required"`
-			BarBarBarFooBar string `json:"bar_bar_bar_foo_bar" yaml:"bar" validate:"required"` // want `tag is not aligned, should be: json:"bar_bar_bar_foo_bar" yaml:"bar"          validate:"required"`
+			BarBarBarFooBar string `json:"bar_bar_bar_foo_bar" yaml:"bar" validate:"required"` // want `json:"bar_bar_bar_foo_bar" yaml:"bar"          validate:"required"`
 		} `json:"bar_bar_bar" validate:"required"`
 	}
 	BarFooBarFoo struct{}
 
-	BarFoo    string `json:"bar_foo" validate:"required"` // want `tag is not aligned, should be: json:"bar_foo"     validate:"required"`
+	BarFoo    string `json:"bar_foo" validate:"required"` // want `json:"bar_foo"     validate:"required"`
 	BarFooBar string `json:"bar_foo_bar" validate:"required"`
 }
 
 type TagAlignSortExampleNoSortExample2 struct {
-	Foo int ` xml:"baz"  json:"foo,omitempty" yaml:"bar"  zip:"foo" validate:"required"`       // want `tag is not aligned, should be: xml:"baz"           json:"foo,omitempty" yaml:"bar" zip:"foo"          validate:"required"`
-	Bar int `validate:"required" yaml:"foo" xml:"bar" binding:"required" json:"bar,omitempty"` // want `tag is not aligned, should be: validate:"required" yaml:"foo"           xml:"bar"  binding:"required" json:"bar,omitempty"`
+	Foo int ` xml:"baz"  json:"foo,omitempty" yaml:"bar"  zip:"foo" validate:"required"`       // want `xml:"baz"           json:"foo,omitempty" yaml:"bar" zip:"foo"          validate:"required"`
+	Bar int `validate:"required" yaml:"foo" xml:"bar" binding:"required" json:"bar,omitempty"` // want `validate:"required" yaml:"foo"           xml:"bar"  binding:"required" json:"bar,omitempty"`
 }
