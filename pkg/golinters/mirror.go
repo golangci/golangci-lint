9b61a82fb834800ea2511f52f81d60a1ca1afce1
Fernandez Ludovic <ldez@users.noreply.github.com>
2023-06-01 11:14:18 +0200

    review

--- pkg/golinters/mirror.go
+++ pkg/golinters/mirror.go
@@ -12,43 +12,43 @@ import (
 )
 
 func NewMirror() *goanalysis.Linter {
-	var ( // Issue reporter related.
+	var (
 		mu     sync.Mutex
 		issues []goanalysis.Issue
 	)
 
 	a := mirror.NewAnalyzer()
 	a.Run = func(pass *analysis.Pass) (any, error) {
-		// mirror only lints test files if the `--with-tests` flag is passed, so we
-		// pass the `with-tests` flag as true to the analyzer before running it. This
-		// can be turned off by using the regular golangci-lint flags such as `--tests`
-		// or `--skip-files` or can be disabled per linter via exclude rules (see
-		// https://github.com/golangci/golangci-lint/issues/2527#issuecomment-1023707262)
-		withTests := true
-		violations := mirror.Run(pass, withTests)
+		// mirror only lints test files if the `--with-tests` flag is passed,
+		// so we pass the `with-tests` flag as true to the analyzer before running it.
+		// This can be turned off by using the regular golangci-lint flags such as `--tests` or `--skip-files`
+		// or can be disabled per linter via exclude rules.
+		// (see https://github.com/golangci/golangci-lint/issues/2527#issuecomment-1023707262)
+		violations := mirror.Run(pass, true)
 
 		if len(violations) == 0 {
 			return nil, nil
 		}
 
-		for i := range violations {
-			tmp := violations[i].Issue(pass.Fset)
+		for index := range violations {
+			i := violations[index].Issue(pass.Fset)
 
 			issue := result.Issue{
 				FromLinter: a.Name,
-				Text:       tmp.Message,
-				Pos:        tmp.Start,
+				Text:       i.Message,
+				Pos:        i.Start,
 			}
 
-			if len(tmp.InlineFix) > 0 {
+			if len(i.InlineFix) > 0 {
 				issue.Replacement = &result.Replacement{
 					Inline: &result.InlineFix{
-						StartCol:  tmp.Start.Column - 1,
-						Length:    len(tmp.Original),
-						NewString: tmp.InlineFix,
+						StartCol:  i.Start.Column - 1,
+						Length:    len(i.Original),
+						NewString: i.InlineFix,
 					},
 				}
 			}
+
 			mu.Lock()
 			issues = append(issues, goanalysis.NewIssue(&issue, pass))
 			mu.Unlock()
