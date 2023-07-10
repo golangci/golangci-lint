dcd876b57e914414a08ec4a172b92d0331538e95
Aldana Longhi <aldulonghi@gmail.com>
2023-07-10 09:33:03 -0300

    move import to previous location

--- scripts/expand_website_templates/main.go
+++ scripts/expand_website_templates/main.go
@@ -18,11 +18,12 @@ import (
 	"strings"
 	"unicode"
 
+	"gopkg.in/yaml.v3"
+
 	"github.com/golangci/golangci-lint/internal/renameio"
 	"github.com/golangci/golangci-lint/pkg/config"
 	"github.com/golangci/golangci-lint/pkg/lint/linter"
 	"github.com/golangci/golangci-lint/pkg/lint/lintersdb"
-	"gopkg.in/yaml.v3"
 )
 
 const listItemPrefix = "list-item-"
