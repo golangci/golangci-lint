66247e7b8c8c0b6cc08a5824a38557713426063d
Vladislav Fursov <github@ghost.im>
2023-10-08 11:34:26 +0400

    protogetter: update tests

--- test/testdata/protogetter/proto/test.go
+++ test/testdata/protogetter/proto/test.go
@@ -10,3 +10,14 @@ type Other struct {
 func (x *Other) MyMethod(certs *Test) *Embedded {
 	return nil
 }
+
+func (x *Test) Equal(v *Test) bool {
+	return false
+}
+
+func (x *Embedded) SetS(s string) {
+	x.S = s
+}
+
+func (x *Embedded) SetMap(_ map[string]string) {
+}
