1f874bec149dce86c39b19a99929d6604a4304b3
Tom <73077675+tmzane@users.noreply.github.com>
2023-10-26 21:39:45 +0300

    build(deps): bump sloglint from 0.1.2 to 0.2.0

---
+++ test/testdata/sloglint_context_only.go
@@ -0,0 +1,16 @@
+//go:build go1.21
+
+//golangcitest:args -Esloglint
+//golangcitest:config_path testdata/configs/sloglint_context_only.yml
+package testdata
+
+import (
+	"context"
+	"log/slog"
+)
+
+func test() {
+	slog.InfoContext(context.Background(), "msg")
+
+	slog.Info("msg") // want `methods without a context should not be used`
+}
