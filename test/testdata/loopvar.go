0025c5a610c1c798c642c46547ad5c1254b2ba69
karamaru-alpha <mrnk3078@gmail.com>
2023-11-05 04:08:05 +0900

    chore: loopvar v1.0.2 to v1.0.3
    Signed-off-by: karamaru-alpha <mrnk3078@gmail.com>

--- test/testdata/loopvar.go
+++ test/testdata/loopvar.go
@@ -9,11 +9,11 @@ func foo() {
 	slice := []int{1, 2, 3}
 	fns := make([]func(), 0, len(slice)*2)
 	for i, v := range slice {
-		i := i // want `The loop variable "i" should not be copied \(GO_VERSION >= 1.22 or GOEXPERIMENT=loopvar\)`
+		i := i // want `The loop variable "i" should not be copied \(Go 1.22~ or Go 1.21 GOEXPERIMENT=loopvar\)`
 		fns = append(fns, func() {
 			fmt.Print(i)
 		})
-		_v := v // want `The loop variable "v" should not be copied \(GO_VERSION >= 1.22 or GOEXPERIMENT=loopvar\)`
+		_v := v // want `The loop variable "v" should not be copied \(Go 1.22~ or Go 1.21 GOEXPERIMENT=loopvar\)`
 		fns = append(fns, func() {
 			fmt.Print(_v)
 		})
@@ -27,7 +27,7 @@ func bar() {
 	loopCount := 3
 	fns := make([]func(), 0, loopCount)
 	for i := 1; i <= loopCount; i++ {
-		i := i // want `The loop variable "i" should not be copied \(GO_VERSION >= 1.22 or GOEXPERIMENT=loopvar\)`
+		i := i // want `The loop variable "i" should not be copied \(Go 1.22~ or Go 1.21 GOEXPERIMENT=loopvar\)`
 		fns = append(fns, func() {
 			fmt.Print(i)
 		})
