327797307613f9a6b86772ce58ad12c744efeeb7
Clifton Kaznocha <clif@impart.security>
2024-02-12 10:58:01 -0800

    feat: add `intrange` linter

---
+++ test/testdata/intrange.go
@@ -0,0 +1,19 @@
+//golangcitest:args -Eintrange
+package testdata
+
+import "math"
+
+func CheckIntrange() {
+	for i := 0; i < 10; i++ { // want `for loop can be changed to use an integer range \(Go 1\.22\+\)`
+	}
+
+	for i := uint8(0); i < math.MaxInt8; i++ { // want `for loop can be changed to use an integer range \(Go 1\.22\+\)`
+	}
+
+	for i := 0; i < 10; i += 2 {
+	}
+
+	for i := 0; i < 10; i++ {
+		i += 1
+	}
+}
