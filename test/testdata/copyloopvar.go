ea900d35d692d962211a25773552dce76a0f6cd8
karamaru-alpha <mrnk3078@gmail.com>
2024-02-14 09:40:32 +0900

    update: add std lib to testcase
    Signed-off-by: karamaru-alpha <mrnk3078@gmail.com>

--- test/testdata/copyloopvar.go
+++ test/testdata/copyloopvar.go
@@ -1,17 +1,19 @@
 //golangcitest:args -Ecopyloopvar
 package testdata
 
+import "fmt"
+
 func copyloopvarCase1() {
 	slice := []int{1, 2, 3}
 	fns := make([]func(), 0, len(slice)*2)
 	for i, v := range slice {
 		i := i // want `It's unnecessary to copy the loop variable "i"`
 		fns = append(fns, func() {
-			_ = i
+			fmt.Println(i)
 		})
 		_v := v // want `It's unnecessary to copy the loop variable "v"`
 		fns = append(fns, func() {
-			_ = _v
+			fmt.Println(_v)
 		})
 	}
 	for _, fn := range fns {
@@ -25,7 +27,7 @@ func copyloopvarCase2() {
 	for i := 1; i <= loopCount; i++ {
 		i := i // want `It's unnecessary to copy the loop variable "i"`
 		fns = append(fns, func() {
-			_ = i
+			fmt.Println(i)
 		})
 	}
 	for _, fn := range fns {
