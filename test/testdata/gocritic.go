c65868c10590fcb63813c3a169e905b837cbed8f
Anton Telyshev <anton.telishev@yandex.ru>
2024-02-19 16:50:25 +0300

    gocritic: support of enable-all and disable-all options (#4335)
    Co-authored-by: Ludovic Fernandez <ldez@users.noreply.github.com>

--- test/testdata/gocritic.go
+++ test/testdata/gocritic.go
@@ -4,6 +4,7 @@ package testdata
 
 import (
 	"flag"
+	"io"
 	"log"
 	"strings"
 )
@@ -11,36 +12,59 @@ import (
 var _ = *flag.Bool("global1", false, "") // want `flagDeref: immediate deref in \*flag.Bool\(.global1., false, ..\) is most likely an error; consider using flag\.BoolVar`
 
 type size1 struct {
-	a bool
+	a [12]bool
 }
 
 type size2 struct {
 	size1
-	b bool
+	b [12]bool
 }
 
-func gocriticRangeValCopySize1(ss []size1) {
-	for _, s := range ss {
-		log.Print(s)
+func gocriticAppendAssign() {
+	var positives, negatives []int
+	positives = append(negatives, 1)
+	negatives = append(negatives, -1)
+	log.Print(positives, negatives)
+}
+
+func gocriticDupSubExpr(x bool) {
+	if x && x { // want "dupSubExpr: suspicious identical LHS and RHS.*"
+		log.Print("x is true")
 	}
 }
 
-func gocriticRangeValCopySize2(ss []size2) {
-	for _, s := range ss { // want "rangeValCopy: each iteration copies 2 bytes.*"
-		log.Print(s)
+func gocriticHugeParamSize1(ss size1) {
+	log.Print(ss)
+}
+
+func gocriticHugeParamSize2(ss size2) { // want "hugeParam: ss is heavy \\(24 bytes\\); consider passing it by pointer"
+	log.Print(ss)
+}
+
+func gocriticHugeParamSize2Ptr(ss *size2) {
+	log.Print(*ss)
+}
+
+func gocriticSwitchTrue() {
+	switch true {
+	case false:
+		log.Print("false")
+	default:
+		log.Print("default")
 	}
+}
+
+func goCriticPreferStringWriter(w interface {
+	io.Writer
+	io.StringWriter
+}) {
+	w.Write([]byte("test")) // want "ruleguard: w\\.WriteString\\(\"test\"\\) should be preferred.*"
 }
 
 func gocriticStringSimplify() {
 	s := "Most of the time, travellers worry about their luggage."
 	s = strings.Replace(s, ",", "", -1) // want "ruleguard: this Replace call can be simplified.*"
 	log.Print(s)
-}
-
-func gocriticDup(x bool) {
-	if x && x { // want "ruleguard: suspicious identical LHS and RHS.*"
-		log.Print("x is true")
-	}
 }
 
 func gocriticRuleWrapperFunc() {
