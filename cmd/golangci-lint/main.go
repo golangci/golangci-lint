871c1c6c004d5d53904acfe2e1f4614eb34dfb4b
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-02-23 00:12:22 +0100

    feat: use the new commands

--- cmd/golangci-lint/main.go
+++ cmd/golangci-lint/main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"runtime/debug"
 
-	"github.com/golangci/golangci-lint/pkg/commands"
+	"github.com/golangci/golangci-lint/pkg/cmder"
 	"github.com/golangci/golangci-lint/pkg/exitcodes"
 )
 
@@ -29,17 +29,15 @@ func main() {
 		}
 	}
 
-	info := commands.BuildInfo{
+	info := cmder.BuildInfo{
 		GoVersion: goVersion,
 		Version:   version,
 		Commit:    commit,
 		Date:      date,
 	}
 
-	e := commands.NewExecutor(info)
-
-	if err := e.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "failed executing command with error %v\n", err)
+	if err := cmder.Execute(info); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "failed executing command with error %v\n", err)
 		os.Exit(exitcodes.Failure)
 	}
 }
