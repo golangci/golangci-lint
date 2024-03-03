df70758a05637121c43f1b25055734b1ae89ba38
Ryosei Karaki <38310693+karamaru-alpha@users.noreply.github.com>
2024-03-03 22:51:16 +0900

    build(deps): bump github.com/karamaru-alpha/copyloopvar from 1.0.4 to 1.0.8 (#4444)

--- pkg/golinters/copyloopvar.go
+++ pkg/golinters/copyloopvar.go
@@ -4,16 +4,26 @@ import (
 	"github.com/karamaru-alpha/copyloopvar"
 	"golang.org/x/tools/go/analysis"
 
+	"github.com/golangci/golangci-lint/pkg/config"
 	"github.com/golangci/golangci-lint/pkg/golinters/goanalysis"
 )
 
-func NewCopyLoopVar() *goanalysis.Linter {
-	a := copyloopvar.Analyzer
+func NewCopyLoopVar(settings *config.CopyLoopVarSettings) *goanalysis.Linter {
+	a := copyloopvar.NewAnalyzer()
+
+	var cfg map[string]map[string]any
+	if settings != nil {
+		cfg = map[string]map[string]any{
+			a.Name: {
+				"ignore-alias": settings.IgnoreAlias,
+			},
+		}
+	}
 
 	return goanalysis.NewLinter(
 		a.Name,
 		a.Doc,
 		[]*analysis.Analyzer{a},
-		nil,
+		cfg,
 	).WithLoadMode(goanalysis.LoadModeSyntax)
 }
