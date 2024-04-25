b2df2f4877bc20ec8e60076e50dccb4b01e84e08
Roman Chaliy <31652715+lasiar@users.noreply.github.com>
2024-04-26 05:12:18 +0700

    Add new linter canonicalheader (#4672)

---
+++ pkg/golinters/canonicalheader/canonicalheader.go
@@ -0,0 +1,19 @@
+package canonicalheader
+
+import (
+	"github.com/lasiar/canonicalheader"
+	"golang.org/x/tools/go/analysis"
+
+	"github.com/golangci/golangci-lint/pkg/goanalysis"
+)
+
+func New() *goanalysis.Linter {
+	a := canonicalheader.Analyzer
+
+	return goanalysis.NewLinter(
+		a.Name,
+		a.Doc,
+		[]*analysis.Analyzer{a},
+		nil,
+	).WithLoadMode(goanalysis.LoadModeTypesInfo)
+}
