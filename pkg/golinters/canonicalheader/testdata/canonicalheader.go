3313f325e10f8a647af3c9c6b11e20c20ebbd249
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-05-10 03:07:57 +0200

    chore: update tests

--- pkg/golinters/canonicalheader/testdata/canonicalheader.go
+++ pkg/golinters/canonicalheader/testdata/canonicalheader.go
@@ -12,6 +12,8 @@ func canonicalheader() {
 	v.Del("Test-HEader")          // want `non-canonical header "Test-HEader", instead use: "Test-Header"`
 	v.Values("Test-HEader")       // want `non-canonical header "Test-HEader", instead use: "Test-Header"`
 
+	v.Values("Sec-WebSocket-Accept")
+
 	v.Set("Test-Header", "value")
 	v.Add("Test-Header", "value")
 	v.Del("Test-Header")
