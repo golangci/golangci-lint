edb6baa505cc3a255fda21a8a6900398496b6791
Anton Telyshev <anton.telishev@yandex.ru>
2024-05-10 12:11:02 +0300

    nilnil: improve tests

--- pkg/golinters/nilnil/testdata/nilnil.go
+++ pkg/golinters/nilnil/testdata/nilnil.go
@@ -2,7 +2,11 @@
 package testdata
 
 import (
+	"bytes"
+	"go/token"
 	"io"
+	"net/http"
+	"os"
 	"unsafe"
 )
 
@@ -22,6 +26,26 @@ func emptyStructPtr() (*struct{}, error) {
 
 func anonymousStructPtr() (*struct{ ID string }, error) {
 	return nil, nil // want "return both the `nil` error and invalid value: use a sentinel error instead"
+}
+
+func unsafePtr() (unsafe.Pointer, error) {
+	return nil, nil // want "return both the `nil` error and invalid value: use a sentinel error instead"
+}
+
+func uintPtr() (uintptr, error) {
+	return 0, nil // want "return both the `nil` error and invalid value: use a sentinel error instead"
+}
+
+func uintPtr0b() (uintptr, error) {
+	return 0b0, nil // want "return both the `nil` error and invalid value: use a sentinel error instead"
+}
+
+func uintPtr0x() (uintptr, error) {
+	return 0x00, nil // want "return both the `nil` error and invalid value: use a sentinel error instead"
+}
+
+func uintPtr0o() (uintptr, error) {
+	return 0o000, nil // want "return both the `nil` error and invalid value: use a sentinel error instead"
 }
 
 func chBi() (chan int, error) {
@@ -48,11 +72,21 @@ func iface() (interface{}, error) {
 	return nil, nil // want "return both the `nil` error and invalid value: use a sentinel error instead"
 }
 
+func anyType() (any, error) {
+	return nil, nil // want "return both the `nil` error and invalid value: use a sentinel error instead"
+}
+
 func m1() (map[int]int, error) {
 	return nil, nil // want "return both the `nil` error and invalid value: use a sentinel error instead"
 }
 
 func m2() (map[int]*User, error) {
+	return nil, nil // want "return both the `nil` error and invalid value: use a sentinel error instead"
+}
+
+type mapAlias = map[int]*User
+
+func m3() (mapAlias, error) {
 	return nil, nil // want "return both the `nil` error and invalid value: use a sentinel error instead"
 }
 
@@ -119,6 +153,39 @@ func deeplyNested() {
 	}
 }
 
+type MyError interface {
+	error
+	Code() string
+}
+
+func myError() (*User, MyError) {
+	return nil, nil // want "return both the `nil` error and invalid value: use a sentinel error instead"
+}
+
+// Types.
+
+func structPtrTypeExtPkg() (*os.File, error) {
+	return nil, nil // want "return both the `nil` error and invalid value: use a sentinel error instead"
+}
+
+func primitivePtrTypeExtPkg() (*token.Token, error) {
+	return nil, nil // want "return both the `nil` error and invalid value: use a sentinel error instead"
+}
+
+func funcTypeExtPkg() (http.HandlerFunc, error) {
+	return nil, nil // want "return both the `nil` error and invalid value: use a sentinel error instead"
+}
+
+func ifaceTypeExtPkg() (io.Closer, error) {
+	return nil, nil // want "return both the `nil` error and invalid value: use a sentinel error instead"
+}
+
+type closerAlias = io.Closer
+
+func ifaceTypeAliasedExtPkg() (closerAlias, error) {
+	return nil, nil // want "return both the `nil` error and invalid value: use a sentinel error instead"
+}
+
 type (
 	StructPtrType    *User
 	PrimitivePtrType *int
@@ -147,21 +214,93 @@ func ifaceType() (Checker, error) {
 	return nil, nil // want "return both the `nil` error and invalid value: use a sentinel error instead"
 }
 
+type checkerAlias = Checker
+
+func ifaceTypeAliased() (checkerAlias, error) {
+	return nil, nil // want "return both the `nil` error and invalid value: use a sentinel error instead"
+}
+
+type (
+	IntegerType    int
+	PtrIntegerType *IntegerType
+)
+
+func ptrIntegerType() (PtrIntegerType, error) {
+	return nil, nil // want "return both the `nil` error and invalid value: use a sentinel error instead"
+}
+
+// Not checked at all.
+
 func withoutArgs()                                {}
 func withoutError1() *User                        { return nil }
 func withoutError2() (*User, *User)               { return nil, nil }
 func withoutError3() (*User, *User, *User)        { return nil, nil, nil }
 func withoutError4() (*User, *User, *User, *User) { return nil, nil, nil, nil }
 
-// Unsupported.
-
 func invalidOrder() (error, *User)               { return nil, nil }
 func withError3rd() (*User, bool, error)         { return nil, false, nil }
 func withError4th() (*User, *User, *User, error) { return nil, nil, nil, nil }
-func unsafePtr() (unsafe.Pointer, error)         { return nil, nil }
-func uintPtr() (uintptr, error)                  { return 0, nil }
-func slice() ([]int, error)                      { return nil, nil }
-func ifaceExtPkg() (io.Closer, error)            { return nil, nil }
+
+func slice() ([]int, error) { return nil, nil }
+
+func strNil() (string, error)   { return "nil", nil }
+func strEmpty() (string, error) { return "", nil }
+
+// Valid.
+
+func primitivePtrTypeValid() (*int, error) {
+	if false {
+		return nil, io.EOF
+	}
+	return new(int), nil
+}
+
+func structPtrTypeValid() (*User, error) {
+	if false {
+		return nil, io.EOF
+	}
+	return new(User), nil
+}
+
+func unsafePtrValid() (unsafe.Pointer, error) {
+	if false {
+		return nil, io.EOF
+	}
+	var i int
+	return unsafe.Pointer(&i), nil
+}
+
+func uintPtrValid() (uintptr, error) {
+	if false {
+		return 0, io.EOF
+	}
+	return 0xc82000c290, nil
+}
+
+func channelTypeValid() (ChannelType, error) {
+	if false {
+		return nil, io.EOF
+	}
+	return make(ChannelType), nil
+}
+
+func funcTypeValid() (FuncType, error) {
+	if false {
+		return nil, io.EOF
+	}
+	return func(i int) int {
+		return 0
+	}, nil
+}
+
+func ifaceTypeValid() (io.Reader, error) {
+	if false {
+		return nil, io.EOF
+	}
+	return new(bytes.Buffer), nil
+}
+
+// Unsupported.
 
 func implicitNil1() (*User, error) {
 	err := (error)(nil)
