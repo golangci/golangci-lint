9cb7fb2f0a6f839e1eda3c7c17efe30f5590e985
Tiago Peczenyj <tpeczenyj@weborama.com>
2024-06-10 10:40:05 +0200

    include preset test

--- pkg/lint/lintersdb/builder_linter.go
+++ pkg/lint/lintersdb/builder_linter.go
@@ -760,7 +760,7 @@ func (LinterBuilder) Build(cfg *config.Config) ([]*linter.Config, error) {
 
 		linter.NewConfig(ttempdir.New(&cfg.LintersSettings.Ttempdir)).
 			WithSince("v1.60.0").
-			WithPresets(linter.PresetStyle).
+			WithPresets(linter.PresetStyle, linter.PresetTest).
 			WithURL("https://github.com/peczenyj/ttempdir"),
 
 		linter.NewConfig(golinters.NewTypecheck()).
