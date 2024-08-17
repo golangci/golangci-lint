e3ec2fec39ab170c5cc3b4c947740491b7f97038
rnben <zhirubin@outlook.com>
2024-08-17 22:56:13 +0800

    fix: labelled break report error

--- pkg/golinters/gobreakselectinfor/testdata/gobreakselectinfor.go
+++ pkg/golinters/gobreakselectinfor/testdata/gobreakselectinfor.go
@@ -1,12 +1,21 @@
 //golangcitest:args -Egobreakselectinfor
 package testdata
 
-func bad() {
-	var ch chan string
+func bad(ch <-chan string) {
 	for {
 		select {
 		case <-ch:
 			break // want "break statement inside select statement inside for loop"
+		}
+	}
+}
+
+func good(ch <-chan string) {
+OUTER:
+	for {
+		select {
+		case <-ch:
+			break OUTER
 		}
 	}
 }
