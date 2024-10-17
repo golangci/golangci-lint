ff101f83719b6b738ce7cf7f01d3a8bb07486178
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-10-17 14:16:23 +0200

    chore: update implementation

--- pkg/golinters/errcheck/testdata/errcheck_type_assertions.go
+++ pkg/golinters/errcheck/testdata/errcheck_type_assertions.go
@@ -1,8 +1,8 @@
 //golangcitest:args -Eerrcheck
 //golangcitest:config_path testdata/errcheck_type_assertions.yml
-//golangcitest:expected_exitcode 0
+//golangcitest:expected_exitcode 1
 package testdata
 
 func ErrorTypeAssertion(filter map[string]interface{}) bool {
-	return filter["messages_sent.messageid"].(map[string]interface{})["$ne"] != nil
+	return filter["messages_sent.messageid"].(map[string]interface{})["$ne"] != nil // want "Error return value is not checked"
 }
