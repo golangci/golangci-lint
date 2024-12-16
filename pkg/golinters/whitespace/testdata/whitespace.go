0dbb927804ac6e0e36d196ea56bd0900482af2ee
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-12-16 01:39:29 +0100

    tests: autofix whitespace

--- pkg/golinters/whitespace/testdata/whitespace.go
+++ pkg/golinters/whitespace/testdata/whitespace.go
@@ -1,16 +1,18 @@
 //golangcitest:args -Ewhitespace
-//golangcitest:config_path testdata/whitespace-fix.yml
-//golangcitest:expected_exitcode 0
-package p
+//golangcitest:config_path testdata/whitespace.yml
+package testdata
 
 import "fmt"
 
 func oneLeadingNewline() {
+
 	fmt.Println("Hello world")
 }
 
 func oneNewlineAtBothEnds() {
+
 	fmt.Println("Hello world")
+
 }
 
 func noNewlineFunc() {
@@ -21,6 +23,7 @@ func oneNewlineFunc() {
 }
 
 func twoNewlinesFunc() {
+
 
 }
 
@@ -39,19 +42,19 @@ func oneLeadingNewlineWithCommentFunc() {
 }
 
 func twoLeadingNewlines() {
+
+
 	fmt.Println("Hello world")
 }
 
 func multiFuncFunc(a int,
 	b int) {
-
 	fmt.Println("Hello world")
 }
 
 func multiIfFunc() {
 	if 1 == 1 &&
 		2 == 2 {
-
 		fmt.Println("Hello multi-line world")
 	}
 
@@ -60,8 +63,7 @@ func multiIfFunc() {
 			if true {
 				if 1 == 1 &&
 					2 == 2 {
-
-					fmt.Println("Hello nested multi-line world")
+						fmt.Println("Hello nested multi-line world")
 				}
 			}
 		}
@@ -69,5 +71,12 @@ func multiIfFunc() {
 }
 
 func notGoFmted() {
-	fmt.Println("Hello world")
+
+
+
+
+         fmt.Println("Hello world")
+
+
+
 }
