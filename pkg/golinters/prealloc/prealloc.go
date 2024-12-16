c7e3c0f6da5b6b99753ba10c629129233ab63820
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-12-16 01:53:27 +0100

    chore: migrate prealloc

--- pkg/golinters/prealloc/prealloc.go
+++ pkg/golinters/prealloc/prealloc.go
@@ -2,7 +2,6 @@ package prealloc
 
 import (
 	"fmt"
-	"sync"
 
 	"github.com/alexkohler/prealloc/pkg"
 	"golang.org/x/tools/go/analysis"
@@ -10,29 +9,16 @@ import (
 	"github.com/golangci/golangci-lint/pkg/config"
 	"github.com/golangci/golangci-lint/pkg/goanalysis"
 	"github.com/golangci/golangci-lint/pkg/golinters/internal"
-	"github.com/golangci/golangci-lint/pkg/lint/linter"
-	"github.com/golangci/golangci-lint/pkg/result"
 )
 
 const linterName = "prealloc"
 
 func New(settings *config.PreallocSettings) *goanalysis.Linter {
-	var mu sync.Mutex
-	var resIssues []goanalysis.Issue
-
 	analyzer := &analysis.Analyzer{
 		Name: linterName,
 		Doc:  goanalysis.TheOnlyanalyzerDoc,
 		Run: func(pass *analysis.Pass) (any, error) {
-			issues := runPreAlloc(pass, settings)
-
-			if len(issues) == 0 {
-				return nil, nil
-			}
-
-			mu.Lock()
-			resIssues = append(resIssues, issues...)
-			mu.Unlock()
+			runPreAlloc(pass, settings)
 
 			return nil, nil
 		},
@@ -43,23 +29,16 @@ func New(settings *config.PreallocSettings) *goanalysis.Linter {
 		"Finds slice declarations that could potentially be pre-allocated",
 		[]*analysis.Analyzer{analyzer},
 		nil,
-	).WithIssuesReporter(func(*linter.Context) []goanalysis.Issue {
-		return resIssues
-	}).WithLoadMode(goanalysis.LoadModeSyntax)
+	).WithLoadMode(goanalysis.LoadModeSyntax)
 }
 
-func runPreAlloc(pass *analysis.Pass, settings *config.PreallocSettings) []goanalysis.Issue {
-	var issues []goanalysis.Issue
-
+func runPreAlloc(pass *analysis.Pass, settings *config.PreallocSettings) {
 	hints := pkg.Check(pass.Files, settings.Simple, settings.RangeLoops, settings.ForLoops)
 
 	for _, hint := range hints {
-		issues = append(issues, goanalysis.NewIssue(&result.Issue{
-			Pos:        pass.Fset.Position(hint.Pos),
-			Text:       fmt.Sprintf("Consider pre-allocating %s", internal.FormatCode(hint.DeclaredSliceName, nil)),
-			FromLinter: linterName,
-		}, pass))
+		pass.Report(analysis.Diagnostic{
+			Pos:     hint.Pos,
+			Message: fmt.Sprintf("Consider pre-allocating %s", internal.FormatCode(hint.DeclaredSliceName, nil)),
+		})
 	}
-
-	return issues
 }
