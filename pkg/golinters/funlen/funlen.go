270dfb48c5e7adfafdec35481072adda69736ddc
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-12-16 13:42:53 +0100

    chore: update implementation

--- pkg/golinters/funlen/funlen.go
+++ pkg/golinters/funlen/funlen.go
@@ -1,75 +1,33 @@
 package funlen
 
 import (
-	"go/token"
-	"strings"
-	"sync"
-
 	"github.com/ultraware/funlen"
 	"golang.org/x/tools/go/analysis"
 
 	"github.com/golangci/golangci-lint/pkg/config"
 	"github.com/golangci/golangci-lint/pkg/goanalysis"
-	"github.com/golangci/golangci-lint/pkg/lint/linter"
-	"github.com/golangci/golangci-lint/pkg/result"
 )
 
-const linterName = "funlen"
-
-func New(settings *config.FunlenSettings) *goanalysis.Linter {
-	var mu sync.Mutex
-	var resIssues []goanalysis.Issue
-
-	analyzer := &analysis.Analyzer{
-		Name: linterName,
-		Doc:  goanalysis.TheOnlyanalyzerDoc,
-		Run: func(pass *analysis.Pass) (any, error) {
-			issues := runFunlen(pass, settings)
-
-			if len(issues) == 0 {
-				return nil, nil
-			}
-
-			mu.Lock()
-			resIssues = append(resIssues, issues...)
-			mu.Unlock()
-
-			return nil, nil
-		},
-	}
-
-	return goanalysis.NewLinter(
-		linterName,
-		"Tool for detection of long functions",
-		[]*analysis.Analyzer{analyzer},
-		nil,
-	).WithIssuesReporter(func(*linter.Context) []goanalysis.Issue {
-		return resIssues
-	}).WithLoadMode(goanalysis.LoadModeSyntax)
+type Config struct {
+	lineLimit      int
+	stmtLimit      int
+	ignoreComments bool
 }
 
-func runFunlen(pass *analysis.Pass, settings *config.FunlenSettings) []goanalysis.Issue {
-	var lintIssues []funlen.Message
-	for _, file := range pass.Files {
-		fileIssues := funlen.Run(file, pass.Fset, settings.Lines, settings.Statements, settings.IgnoreComments)
-		lintIssues = append(lintIssues, fileIssues...)
+func New(settings *config.FunlenSettings) *goanalysis.Linter {
+	cfg := Config{}
+	if settings != nil {
+		cfg.lineLimit = settings.Lines
+		cfg.stmtLimit = settings.Statements
+		cfg.ignoreComments = !settings.IgnoreComments
 	}
 
-	if len(lintIssues) == 0 {
-		return nil
-	}
+	a := funlen.NewAnalyzer(cfg.lineLimit, cfg.stmtLimit, cfg.ignoreComments)
 
-	issues := make([]goanalysis.Issue, len(lintIssues))
-	for k, i := range lintIssues {
-		issues[k] = goanalysis.NewIssue(&result.Issue{
-			Pos: token.Position{
-				Filename: i.Pos.Filename,
-				Line:     i.Pos.Line,
-			},
-			Text:       strings.TrimRight(i.Message, "\n"),
-			FromLinter: linterName,
-		}, pass)
-	}
-
-	return issues
+	return goanalysis.NewLinter(
+		a.Name,
+		a.Doc,
+		[]*analysis.Analyzer{a},
+		nil,
+	).WithLoadMode(goanalysis.LoadModeSyntax)
 }
