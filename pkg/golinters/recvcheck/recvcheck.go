044ef20e9e89928e477296f221183619b911f3c1
Fernandez Ludovic <ldez@users.noreply.github.com>
2024-12-26 17:43:17 +0100

    chore: update implementation

--- pkg/golinters/recvcheck/recvcheck.go
+++ pkg/golinters/recvcheck/recvcheck.go
@@ -4,11 +4,19 @@ import (
 	"github.com/raeperd/recvcheck"
 	"golang.org/x/tools/go/analysis"
 
+	"github.com/golangci/golangci-lint/pkg/config"
 	"github.com/golangci/golangci-lint/pkg/goanalysis"
 )
 
-func New() *goanalysis.Linter {
-	a := recvcheck.Analyzer
+func New(settings *config.RecvcheckSettings) *goanalysis.Linter {
+	var cfg recvcheck.Settings
+
+	if settings != nil {
+		cfg.DisableBuiltin = settings.DisableBuiltin
+		cfg.Exclusions = settings.Exclusions
+	}
+
+	a := recvcheck.NewAnalyzer(cfg)
 
 	return goanalysis.NewLinter(
 		a.Name,
