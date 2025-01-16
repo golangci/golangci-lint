1ec4dad46b23c9fa96ac18d70ae7ff343a9b0fa8
Gabriel Augendre <gabriel@augendre.info>
2025-01-16 23:55:39 +0100

    feat: fatcontext settings

--- pkg/golinters/fatcontext/fatcontext.go
+++ pkg/golinters/fatcontext/fatcontext.go
@@ -4,16 +4,25 @@ import (
 	"github.com/Crocmagnon/fatcontext/pkg/analyzer"
 	"golang.org/x/tools/go/analysis"
 
+	"github.com/golangci/golangci-lint/pkg/config"
 	"github.com/golangci/golangci-lint/pkg/goanalysis"
 )
 
-func New() *goanalysis.Linter {
-	a := analyzer.Analyzer
+func New(settings *config.FatcontextSettings) *goanalysis.Linter {
+	a := analyzer.NewAnalyzer()
+
+	cfg := map[string]map[string]any{}
+
+	if settings != nil {
+		cfg[a.Name] = map[string]any{
+			analyzer.FlagCheckStructPointers: settings.CheckStructPointers,
+		}
+	}
 
 	return goanalysis.NewLinter(
 		a.Name,
 		a.Doc,
 		[]*analysis.Analyzer{a},
-		nil,
+		cfg,
 	).WithLoadMode(goanalysis.LoadModeTypesInfo)
 }
