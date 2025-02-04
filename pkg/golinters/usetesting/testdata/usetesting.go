7553a1ae434fb6486d9e74a57c6e30fc6018d30c
Fernandez Ludovic <ldez@users.noreply.github.com>
2025-02-04 03:44:02 +0100

    review

--- pkg/golinters/usetesting/testdata/usetesting.go
+++ pkg/golinters/usetesting/testdata/usetesting.go
@@ -11,7 +11,7 @@ func Test_osMkdirTemp(t *testing.T) {
 }
 
 func Test_osSetenv(t *testing.T) {
-	os.Setenv("", "") // want `s\.Setenv\(\) could be replaced by t\.Setenv\(\) in .+`
+	os.Setenv("", "") // want `os\.Setenv\(\) could be replaced by t\.Setenv\(\) in .+`
 }
 
 func Test_osTempDir(t *testing.T) {
