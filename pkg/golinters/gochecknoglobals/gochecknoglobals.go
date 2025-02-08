e985a1d411586f5447a50639791e6128671fa214
Fernandez Ludovic <ldez@users.noreply.github.com>
2025-02-08 18:31:25 +0100

    chore: update implementation

--- pkg/golinters/gochecknoglobals/gochecknoglobals.go
+++ pkg/golinters/gochecknoglobals/gochecknoglobals.go
@@ -10,17 +10,10 @@ import (
 func New() *goanalysis.Linter {
 	a := checknoglobals.Analyzer()
 
-	// gochecknoglobals only lints test files if the `-t` flag is passed,
-	// so we pass the `t` flag as true to the analyzer before running it.
-	// This can be turned off by using the regular golangci-lint flags such as `--tests` or `--exclude-files`.
-	linterConfig := map[string]map[string]any{
-		a.Name: {"t": true},
-	}
-
 	return goanalysis.NewLinter(
 		a.Name,
 		"Check that no global variables exist.",
 		[]*analysis.Analyzer{a},
-		linterConfig,
+		nil,
 	).WithLoadMode(goanalysis.LoadModeTypesInfo)
 }
