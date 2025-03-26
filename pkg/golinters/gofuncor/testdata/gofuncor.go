944a3f62df3bb9ef65fb81ef7f481ea8aa943188
Manuel Doncel Martos <manuel.doncel.martos@ing.com>
2025-03-26 20:05:09 +0100

    newlinter: adding gofuncor integration test

--- pkg/golinters/gofuncor/testdata/gofuncor.go
+++ pkg/golinters/gofuncor/testdata/gofuncor.go
@@ -1,3 +1,4 @@
+//golangcitest:args -Egofuncor
 package testdata
 
 import "time"
@@ -6,7 +7,7 @@ type MyStruct struct {
 	Name string
 }
 
-func (m MyStruct) lenName() int { // want `unexported method "lenName" for struct "MyStruct" should be placed after the exported method "GetName"`
+func (m MyStruct) lenName() int { // want `unexported method "lenName" for struct "MyStruct" should be placed after the exported method "SetName"`
 	return len(m.Name)
 }
 
