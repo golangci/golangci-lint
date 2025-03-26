5d1fb67494b75408d84b36f153ef8fa69df1be88
Manuel Doncel Martos <manuel.doncel.martos@ing.com>
2025-03-26 20:01:26 +0100

    newlinter: adding gofuncor integration test

---
+++ pkg/golinters/gofuncor/gofuncor_integration_test.go
@@ -0,0 +1,10 @@
+package gofuncor
+
+import (
+	"github.com/golangci/golangci-lint/v2/test/testshared/integration"
+	"testing"
+)
+
+func TestFromTestdata(t *testing.T) {
+	integration.RunTestdata(t)
+}
