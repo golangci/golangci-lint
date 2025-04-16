43e07c47273e04c4d721ce3fef80562ab3333959
sivchari <shibuuuu5@gmail.com>
2025-04-16 23:46:05 +0900

    fix: order of staticcheck settings during migration (#5741)
    Co-authored-by: Fernandez Ludovic <ldez@users.noreply.github.com>

--- pkg/commands/internal/migrate/migrate_linters_settings.go
+++ pkg/commands/internal/migrate/migrate_linters_settings.go
@@ -789,10 +789,32 @@ func toSpancheckSettings(old versionone.SpancheckSettings) versiontwo.SpancheckS
 }
 
 func toStaticCheckSettings(old versionone.LintersSettings) versiontwo.StaticCheckSettings {
-	checks := slices.Concat(old.Staticcheck.Checks, old.Stylecheck.Checks, old.Gosimple.Checks)
+	var checks []string
+
+	for _, check := range slices.Concat(old.Staticcheck.Checks, old.Stylecheck.Checks, old.Gosimple.Checks) {
+		if check == "*" {
+			checks = append(checks, "all")
+			continue
+		}
+		checks = append(checks, check)
+	}
+
+	checks = Unique(checks)
+
+	slices.SortFunc(checks, func(a, b string) int {
+		if a == "all" {
+			return -1
+		}
+
+		if b == "all" {
+			return 1
+		}
+
+		return strings.Compare(a, b)
+	})
 
 	return versiontwo.StaticCheckSettings{
-		Checks:                  Unique(checks),
+		Checks:                  checks,
 		Initialisms:             old.Stylecheck.Initialisms,
 		DotImportWhitelist:      old.Stylecheck.DotImportWhitelist,
 		HTTPStatusCodeWhitelist: old.Stylecheck.HTTPStatusCodeWhitelist,
