ba73ed0d2226df49ec27f667723c9b10d4d36505
Fernandez Ludovic <ldez@users.noreply.github.com>
2025-04-21 13:30:36 +0200

    chore: generate

--- pkg/commands/internal/migrate/versiontwo/output.go
+++ pkg/commands/internal/migrate/versiontwo/output.go
@@ -5,6 +5,7 @@ package versiontwo
 type Output struct {
 	Formats    Formats  `yaml:"formats,omitempty" toml:"formats,multiline,omitempty"`
 	SortOrder  []string `yaml:"sort-order,omitempty" toml:"sort-order,multiline,omitempty"`
-	PathPrefix *string  `yaml:"path-prefix,omitempty" toml:"path-prefix,multiline,omitempty"`
 	ShowStats  *bool    `yaml:"show-stats,omitempty" toml:"show-stats,multiline,omitempty"`
+	PathPrefix *string  `yaml:"path-prefix,omitempty" toml:"path-prefix,multiline,omitempty"`
+	PathMode   *string  `yaml:"path-mode,omitempty" toml:"path-mode,multiline,omitempty"`
 }
