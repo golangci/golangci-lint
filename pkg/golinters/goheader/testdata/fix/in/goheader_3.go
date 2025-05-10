0007283ac56db72a679759016fe88c2b0f0f7086
Denis Tingaikin <denis.tingaikin@outlook.com>
2025-05-10 21:20:15 +0300

    update go header to v1.0.0-rc.7
    Signed-off-by: Denis Tingaikin <denis.tingaikin@outlook.com>

--- pkg/golinters/goheader/testdata/fix/in/goheader_3.go
+++ pkg/golinters/goheader/testdata/fix/in/goheader_3.go
@@ -1,8 +1,8 @@
 /*
-Copyright 1999 The Awesome
-
-Use of this source code is governed by LICENSE
-*/
+ * Copyright 1999 The Awesome
+ *
+ * Use of this source code is governed by LICENSE
+ */
 
 //golangcitest:args -Egoheader
 //golangcitest:expected_exitcode 0
