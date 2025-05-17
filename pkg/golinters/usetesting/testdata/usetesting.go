a2ef9a0a5411f2110f1f7a5a7b686a9bdcf70b05
Fernandez Ludovic <ldez@users.noreply.github.com>
2025-05-17 03:50:35 +0200

    tests: clean go1.24 build tags

--- pkg/golinters/usetesting/testdata/usetesting.go
+++ pkg/golinters/usetesting/testdata/usetesting.go
@@ -2,9 +2,22 @@
 package testdata
 
 import (
+	"context"
 	"os"
 	"testing"
 )
+
+func Test_contextBackground(t *testing.T) {
+	context.Background() // want `context\.Background\(\) could be replaced by t\.Context\(\) in .+`
+}
+
+func Test_contextTODO(t *testing.T) {
+	context.TODO() // want `context\.TODO\(\) could be replaced by t\.Context\(\) in .+`
+}
+
+func Test_osChdir(t *testing.T) {
+	os.Chdir("") // want `os\.Chdir\(\) could be replaced by t\.Chdir\(\) in .+`
+}
 
 func Test_osMkdirTemp(t *testing.T) {
 	os.MkdirTemp("", "") // want `os\.MkdirTemp\(\) could be replaced by t\.TempDir\(\) in .+`
--- pkg/golinters/usetesting/testdata/usetesting_configuration.go
+++ pkg/golinters/usetesting/testdata/usetesting_configuration.go
@@ -3,12 +3,29 @@
 package testdata
 
 import (
+	"context"
 	"os"
 	"testing"
 )
 
+func Test_contextBackground(t *testing.T) {
+	context.Background()
+}
+
+func Test_contextTODO(t *testing.T) {
+	context.TODO()
+}
+
+func Test_osChdir(t *testing.T) {
+	os.Chdir("")
+}
+
 func Test_osMkdirTemp(t *testing.T) {
 	os.MkdirTemp("", "")
+}
+
+func Test_osSetenv(t *testing.T) {
+	os.Setenv("", "") // want `os\.Setenv\(\) could be replaced by t\.Setenv\(\) in .+`
 }
 
 func Test_osTempDir(t *testing.T) {
