f771ef0bc27c461b3a503a053b4ca904170aeb34
Alwx <alwxsin@gmail.com>
2025-05-23 14:04:29 +0300

    add noinlineerr linter

---
+++ pkg/golinters/noinlineerr/testdata/noinlineerr.go
@@ -0,0 +1,52 @@
+//golangcitest:args -Enoinlineerr
+package testdata
+
+func doSomething() error {
+	return nil
+}
+
+func doSmthManyArgs(a, b, c, d int) error {
+	return nil
+}
+
+func doSmthMultipleReturn() (bool, error) {
+	return false, nil
+}
+
+func valid() error {
+	err := doSomething() // ok
+	if err != nil {
+		return err
+	}
+
+	err = doSmthManyArgs(0, 0, 0, 0) // ok
+	if err != nil {
+		return err
+	}
+
+	_, err = doSmthMultipleReturn() // ok
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func invalid() error {
+	if err := doSomething(); err != nil { // want "avoid inline error handling using `if err := ...; err != nil; use plain assignment `err := ..."
+		return err
+	}
+
+	if err := doSmthManyArgs(0, // want "avoid inline error handling using `if err := ...; err != nil; use plain assignment `err := ..."
+		0,
+		0,
+		0,
+	); err != nil {
+		return err
+	}
+
+	if _, err := doSmthMultipleReturn(); err != nil { // want "avoid inline error handling using `if err := ...; err != nil; use plain assignment `err := ..."
+		_ = false
+		return err
+	}
+	return nil
+}
