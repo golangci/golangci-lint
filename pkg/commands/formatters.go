d4bc40ed76ec038911cb56bacdbae38565dd3f8f
ccoVeille <3875889+ccoVeille@users.noreply.github.com>
2025-05-27 12:02:31 +0200

    fix: formatters CLI flags help message (#5835)

--- pkg/commands/formatters.go
+++ pkg/commands/formatters.go
@@ -59,7 +59,8 @@ func newFormattersCommand(logger logutils.Log) *formattersCommand {
 	fs.SortFlags = false // sort them as they are defined here
 
 	setupConfigFileFlagSet(fs, &c.opts.LoaderOptions)
-	setupLintersFlagSet(c.viper, fs)
+
+	setupFormattersFlagSet(c.viper, fs)
 
 	fs.BoolVar(&c.opts.JSON, "json", false, color.GreenString("Display as JSON"))
 
