973d9878f0da3848b92d4ae528ad1d03b30b42f0
Fernandez Ludovic <ldez@users.noreply.github.com>
2025-05-29 15:00:22 +0200

    review

--- pkg/golinters/noinlineerr/noinlineerr.go
+++ pkg/golinters/noinlineerr/noinlineerr.go
@@ -2,17 +2,12 @@ package noinlineerr
 
 import (
 	"github.com/AlwxSin/noinlineerr"
-	"golang.org/x/tools/go/analysis"
 
 	"github.com/golangci/golangci-lint/v2/pkg/goanalysis"
 )
 
 func New() *goanalysis.Linter {
-	analyzer := noinlineerr.NewAnalyzer()
-	return goanalysis.NewLinter(
-		analyzer.Name,
-		analyzer.Doc,
-		[]*analysis.Analyzer{analyzer},
-		nil,
-	).WithLoadMode(goanalysis.LoadModeTypesInfo)
+	return goanalysis.
+		NewLinterFromAnalyzer(noinlineerr.NewAnalyzer()).
+		WithLoadMode(goanalysis.LoadModeTypesInfo)
 }
