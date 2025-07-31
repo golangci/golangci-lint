80fb42f151809c70318514e434d890d4d6104bc3
Fernandez Ludovic <ldez@users.noreply.github.com>
2025-07-31 12:06:18 +0200

    chore: remove old doc

--- docs/gatsby-ssr.js
+++
@@ -1,2 +0,0 @@
-export { wrapRootElement } from "./src/@rocketseat/gatsby-theme-docs/gatsby/wrapRootElement";
-export { wrapPageElement } from "./src/@rocketseat/gatsby-theme-docs/gatsby/wrapPageElement";
