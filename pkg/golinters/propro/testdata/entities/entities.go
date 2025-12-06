145ef399807985f654e9aaf57d131306d6e20b87
Lubomir Cvrk <digitalstraw@gmail.com>
2025-12-06 13:29:18 +0100

    Updated to version 1.2.0, fixed tests.

--- pkg/golinters/propro/testdata/entities/entities.go
+++ pkg/golinters/propro/testdata/entities/entities.go
@@ -1,5 +1,7 @@
 package entities
 
-var EnetityList = []string{
-	"Entity",
+import "github.com/golangci/golangci-lint/v2/pkg/golinters/propro/testdata"
+
+var EntityList = []any{
+	&testdata.Entity{},
 }
