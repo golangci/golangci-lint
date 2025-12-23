372abfc898303365dc3b2c5eab95073b83bdbe3d
Fernandez Ludovic <ldez@users.noreply.github.com>
2025-12-23 14:46:14 +0100

    docs: use new URL

--- scripts/bench/bench_local.sh
+++ scripts/bench/bench_local.sh
@@ -33,7 +33,7 @@ trap cleanBinaries EXIT
 
 ## Download version
 
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/HEAD/install.sh | sh -s -- -b "./temp-${VERSION}" "${VERSION}"
+curl -sSfL https://golangci-lint.run/install.sh | sh -s -- -b "./temp-${VERSION}" "${VERSION}"
 
 mv "temp-${VERSION}/golangci-lint" "./golangci-lint-${VERSION}"
 rm -rf "temp-${VERSION}"
