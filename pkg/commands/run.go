74aa0be29415ac7427995101de3f8d59521e61b9
Fernandez Ludovic <ldez@users.noreply.github.com>
2026-08-03 18:27:13 +0200

    Revert "feat: decrease cache entropy (#6445)"
    This reverts commit 2da081b8b349b1b702a26dcfb3fb4ec720cae4e9.

--- pkg/commands/run.go
+++ pkg/commands/run.go
@@ -713,8 +713,7 @@ func computeGoModSalt() (string, error) {
 		return "", fmt.Errorf("failed to read go.mod: %w", err)
 	}
 
-	// NOTE: the variable `goModPath` is not used here to ensure getting the same hash, independently of the location, for the same content.
-	sum, err := dirhash.Hash1([]string{"go.mod"}, func(string) (io.ReadCloser, error) {
+	sum, err := dirhash.Hash1([]string{goModPath}, func(string) (io.ReadCloser, error) {
 		return io.NopCloser(bytes.NewReader(data)), nil
 	})
 	if err != nil {
